1807c9f61588f03247a6f00352ac4c37c248aee2
Trevor Hickey <thickey@f2si.net>
2015-08-09 18:28:37 -0400

    updates

--- project/src/enums.hpp
+++ project/src/enums.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+//enums to use in place of strings to make code more strongly typed
 enum class Class_Access   { PUBLIC, PROTECTED, PRIVATE };
 enum class Value_Overload { LVALUE, RVALUE };
 enum class Storage_Specifier {STATIC, EXTERN };
