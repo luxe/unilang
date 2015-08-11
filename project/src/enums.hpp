fc797a943a2f10235314e00156f6347cfa89495e
Trevor Hickey <thickey@f2si.net>
2015-08-11 02:47:55 -0400

    updates

--- project/src/enums.hpp
+++ project/src/enums.hpp
@@ -5,4 +5,7 @@ enum class Class_Access   { PUBLIC, PROTECTED, PRIVATE };
 enum class Value_Overload { LVALUE, RVALUE, NONE };
 enum class Storage_Specifier {STATIC, EXTERN, NONE };
 
-
+//concepts not keywords.
+//they are applicable, but not a part of the language.
+enum class State_Specifier {OBSERVABLE,INJECTABLE,OBTAINABLE};
+enum class Function_Category {OBSERVE,TRANSFORM};
