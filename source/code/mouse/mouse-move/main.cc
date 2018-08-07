c783a565f0a89f1e225b8d3eb6f9304ac880092e
Trevor Hickey <TrevorJamesHickey@gmail.com>
2018-08-07 06:53:04 -0700

    updates

---
+++ source/code/mouse/mouse-move/main.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include "code/utilities/mouse/mouse_controller.hpp"
+
+
+//add points to move to and then speed at which to move
+//mouse_move 0 0 2500
+//will slowly move the mouse to the corner of the screen
+int main(int argc, char** argv){
+
+	//get cli args
+	int x_location = atoi(argv[1]);
+	int y_location = atoi(argv[2]);
+	int speed = atoi(argv[3]);
+
+	//move mouse to top left of screen
+	Point p;
+	p.x = x_location;
+	p.y = y_location;
+	Mouse_Controller controller;
+	controller.move_mouse(p,speed);
+}
