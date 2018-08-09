9340daf93b11f8c845a7561953ccdf5030ecb411
Trevor Hickey <TrevorJamesHickey@gmail.com>
2018-08-09 10:40:33 -0700

    updates

--- source/code/mouse/presentations/annotation_tool_summary.cc
+++ source/code/mouse/presentations/annotation_tool_summary.cc
@@ -12,12 +12,14 @@ int main(int argc, char** argv){
     p.x = 675;
     p.y = 472;
     controller.move_mouse_at_presentation_speed(p);
+    Sleep_For_N_Milliseconds(200);
     controller.presentation_click();
     
     //click "Name Box"
     p.x = 726;
     p.y = 568;
     controller.move_mouse_at_presentation_speed(p);
+    Sleep_For_N_Milliseconds(200);
     controller.presentation_click();
     
     // type into "Name Box"
@@ -28,25 +30,82 @@ int main(int argc, char** argv){
     p.x = 734;
     p.y = 596;
     controller.move_mouse_at_presentation_speed(p);
+    Sleep_For_N_Milliseconds(200);
     controller.presentation_click();
-    Sleep_For_N_Milliseconds(600);
+    Sleep_For_N_Milliseconds(500);
     controller.presentation_click();
-    Sleep_For_N_Milliseconds(600);
+    Sleep_For_N_Milliseconds(500);
     controller.presentation_click();
-    Sleep_For_N_Milliseconds(600);
+    Sleep_For_N_Milliseconds(500);
     controller.presentation_click();
-    Sleep_For_N_Milliseconds(600);
+    Sleep_For_N_Milliseconds(500);
     controller.presentation_click();
-    Sleep_For_N_Milliseconds(600);
+    Sleep_For_N_Milliseconds(500);
     controller.presentation_click();
-    Sleep_For_N_Milliseconds(600);
+    Sleep_For_N_Milliseconds(500);
     controller.presentation_click();
-    Sleep_For_N_Milliseconds(600);
+    Sleep_For_N_Milliseconds(500);
     controller.presentation_click();
     
     //click marking button
     p.x = 892;
     p.y = 574;
     controller.move_mouse_at_presentation_speed(p);
+    Sleep_For_N_Milliseconds(200);
+    controller.presentation_click();
+    
+    //toggle start marking
+    p.x = 860;
+    p.y = 812;
+    controller.move_mouse_at_presentation_speed(p);
+    Sleep_For_N_Milliseconds(200);
+    controller.presentation_click();
+    Sleep_For_N_Milliseconds(700);
+    controller.presentation_click();
+    Sleep_For_N_Milliseconds(200);
+    
+    //toggle end marking
+    p.x = 1009;
+    p.y = 812;
+    controller.move_mouse_at_presentation_speed(p);
+    Sleep_For_N_Milliseconds(200);
+    controller.presentation_click();
+    Sleep_For_N_Milliseconds(700);
+    controller.presentation_click();
+    Sleep_For_N_Milliseconds(200);
+    
+    //turn on Regeneration SLA
+    p.x = 860;
+    p.y = 994;
+    controller.move_mouse_at_presentation_speed(p);
+    Sleep_For_N_Milliseconds(200);
+    controller.presentation_click();
+    Sleep_For_N_Milliseconds(200);
+    
+    //hover over sla slider
+    p.x = 1060;
+    p.y = 994;
+    controller.move_mouse_at_presentation_speed(p);
+    Sleep_For_N_Milliseconds(200);
+    
+    //slide left
+    p.x = 1034;
+    p.y = 994;
+    controller.drag_mouse_at_presentation_speed(p);
+    Sleep_For_N_Milliseconds(200);
+    
+    //slide right
+    p.x = 1112;
+    p.y = 994;
+    controller.drag_mouse_at_presentation_speed(p);
+    Sleep_For_N_Milliseconds(200);
+    
+    //close algorithm tab
+    p.x = 1112;
+    p.y = 771;
+    controller.drag_mouse_at_presentation_speed(p);
+    Sleep_For_N_Milliseconds(200);
+    controller.move_mouse_at_presentation_speed(p);
+    Sleep_For_N_Milliseconds(200);
     controller.presentation_click();
 }
