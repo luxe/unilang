bcf9a5cd2e4b82af848087a0271225af593f551b
Trevor Hickey <TrevorJamesHickey@gmail.com>
2018-08-09 14:20:48 -0700

    updates

--- source/code/mouse/presentations/annotation_tool_summary.cc
+++ source/code/mouse/presentations/annotation_tool_summary.cc
@@ -22,7 +22,7 @@ void create_presentation_text(Point size, Point p, std::string const& str){
     command += std::to_string(p.x);
     command += "+";
     command += std::to_string(p.y);
-    command += "-f ";
+    command += " -f ";
     command += "\"";
     command += "Arial Black-24";
     command += "\"";
