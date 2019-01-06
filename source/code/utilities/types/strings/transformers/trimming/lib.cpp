cf946deba9d7589586515812376682cae05e1606
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-01-06 17:25:19 -0500

    updates

--- source/code/utilities/types/strings/transformers/trimming/lib.cpp
+++ source/code/utilities/types/strings/transformers/trimming/lib.cpp
@@ -4,14 +4,15 @@
 #include "code/utilities/std_hackery/lib.hpp"
 #include "code/utilities/language_basic/control_structures/looping/lib.hpp"
 #include <algorithm>
+#include <functional>
 
 //transforming string functions
 std::string &ltrim(std::string &s) {
-        s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) {return !std::isspace(c);}));
         return s;
 }
 std::string &rtrim(std::string &s) {
-        s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+        s.erase(std::find_if(s.rbegin(), s.rend(), [](int c) {return !std::isspace(c);}).base(), s.end());
         return s;
 }
 std::string &trim(std::string &s) {
@@ -24,11 +25,11 @@ std::string& Trim_Each_Line(std::string &s){
         return s;
 }
 std::string As_Left_Trimmed(std::string s){
-        s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) {return !std::isspace(c);}));
         return s;
 }
 std::string As_Right_Trimmed(std::string s){
-        s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+        s.erase(std::find_if(s.rbegin(), s.rend(), [](int c) {return !std::isspace(c);}).base(), s.end());
         return s;
 }
 std::string As_Trimmed(std::string s){
