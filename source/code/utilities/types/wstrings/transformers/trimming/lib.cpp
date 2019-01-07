11ce96df4e774895a031cd26f3b88148ba9ba6fc
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-01-06 20:19:00 -0500

    updates

--- source/code/utilities/types/wstrings/transformers/trimming/lib.cpp
+++ source/code/utilities/types/wstrings/transformers/trimming/lib.cpp
@@ -2,11 +2,11 @@
 #include <algorithm>
 
 std::wstring &ltrim(std::wstring &s) {
-        s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) {return !std::isspace(c);}));
         return s;
 }
 std::wstring &rtrim(std::wstring &s) {
-        s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+        s.erase(std::find_if(s.rbegin(), s.rend(), [](int c) {return !std::isspace(c);}).base(), s.end());
         return s;
 }
 std::wstring &trim(std::wstring &s) {
