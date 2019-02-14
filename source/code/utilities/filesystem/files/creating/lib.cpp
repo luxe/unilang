784fec70e1733a13279f67de8f2e17c242a13765
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-02-14 11:51:52 -0500

    updates

--- source/code/utilities/filesystem/files/creating/lib.cpp
+++ source/code/utilities/filesystem/files/creating/lib.cpp
@@ -1,8 +1,14 @@
 #include "code/utilities/filesystem/files/creating/lib.hpp"
 #include "code/utilities/filesystem/paths/lib.hpp"
 #include "code/utilities/types/strings/transformers/stripping/lib.hpp"
+#include <fstream>
 
 void Create_File_Even_If_The_Path_Doesnt_Exist(std::string const& path_to_file){
   auto path = Get_File_Path_Without_The_Filename(path_to_file);
   Create_Path_If_It_Doesnt_Already_Exist(path);
+}
+void Write_To_File(std::string path_to_file, std::string content)
+{
+	std::ofstream outfile(path_to_file);
+	outfile << content;
 }
--- source/code/utilities/filesystem/files/creating/lib.hpp
+++ source/code/utilities/filesystem/files/creating/lib.hpp
@@ -2,3 +2,4 @@
 #include <string>
 
 void Create_File_Even_If_The_Path_Doesnt_Exist(std::string const& path_to_file);
+void Write_To_File(std::string path_to_file, std::string content);
