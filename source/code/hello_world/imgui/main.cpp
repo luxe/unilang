364469881d9451fdc6ba6e124a23ebc257389093
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-03-01 10:52:02 -0500

    updates

--- source/code/hello_world/imgui/main.cpp
+++ source/code/hello_world/imgui/main.cpp
@@ -1,9 +1,13 @@
 #include "code/utilities/graphics/imgui/mechanics.hpp"
+#include "code/utilities/graphics/imgui/sdl_init_settings.hpp"
 
 int main()
 {
 
+    Sdl_Init_Settings s;
+    s.main_window.title = "hello world";
+    
     auto before = [](){};
     auto during = [](){};
-    return Mechanics::render_each_frame("hello world",before,during);
+    return Mechanics::render_each_frame(s,before,during);
 }
