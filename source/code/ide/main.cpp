1dc16ee16c0f8a21d25a70f67b1c6ce5bc01e9dd
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-03-01 14:26:28 -0500

    updates

--- source/code/ide/main.cpp
+++ source/code/ide/main.cpp
@@ -1,6 +1,12 @@
 #include "code/utilities/graphics/imgui/mechanics.hpp"
 #include "code/utilities/graphics/imgui/sdl_init_settings.hpp"
+#include "code/utilities/graphics/imgui/ui/ui_renders.hpp"
 
+void during_func(){
+    
+    Ui_Renders::render_main_menu_bar([](){
+    });
+}
 int main()
 {
 
@@ -8,6 +14,6 @@ int main()
     s.main_window.title = "Unilang IDE";
     
     auto before = [](){};
-    auto during = [](){};
+    auto during = during_func;
     return Mechanics::render_each_frame(s,before,during);
 }
