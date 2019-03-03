954ab3c5dab7ffe8487bf546edb8ed474333f66c
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-03-03 12:01:56 -0500

    updates

--- source/code/hello_world/serialization/int.cpp
+++ source/code/hello_world/serialization/int.cpp
@@ -4,8 +4,12 @@
 #include <string>
 #include <utility>
 #include "cereal/archives/binary.hpp"
+#define _LIBCPP_ENABLE_CXX17_REMOVED_AUTO_PTR
+#define BOOST_NO_AUTO_PTR
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
 
-TEST(HelloTest, IntTest) {
+TEST(HelloTest, CerealInt) {
 	
 	std::stringstream ss;
 	{
@@ -20,3 +24,20 @@ TEST(HelloTest, IntTest) {
 		EXPECT_EQ(i,5);
 	}
 }
+
+TEST(HelloTest, BoostInt) {
+	
+	std::stringstream ss;
+	{
+		boost::archive::text_oarchive oarchive(ss);
+		int i = 5;
+		oarchive << i;
+	}
+	{
+		boost::archive::text_iarchive iarchive(ss);
+		int i = 0;
+		iarchive >> i;
+		EXPECT_EQ(i,5);
+	}
+}
+
