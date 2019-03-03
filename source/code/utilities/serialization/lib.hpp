6a95a96a33a588b6c3e07d7dac92de5639ee9443
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-03-03 13:01:19 -0500

    updates

--- source/code/utilities/serialization/lib.hpp
+++ source/code/utilities/serialization/lib.hpp
@@ -1,15 +1,18 @@
 #pragma once
 #include <fstream>
+#define _LIBCPP_ENABLE_CXX17_REMOVED_AUTO_PTR
+#define BOOST_NO_AUTO_PTR
 #include "boost/archive/text_oarchive.hpp"
 #include "boost/archive/text_iarchive.hpp"
 
+
+//file
 template<typename T>
 void Load_Object_From_Boost_Serialized_Data_File(T & obj, std::string const& file_name){
     std::ifstream infile(file_name);
     std::unique_ptr<boost::archive::text_iarchive> arc(new boost::archive::text_iarchive(infile));
         *arc >> obj;
 }
-
 template<typename T>
 void Save_Object_To_A_Boost_Serliazed_Data_File(T const& obj, std::string const& file_name){
     std::ofstream outfile(file_name);
