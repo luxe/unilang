d32019f70818a23eb0b5b674eeb64125529c3b28
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-03-03 01:06:27 -0500

    updates

--- source/bazel/deps/build_files/cereal.BUILD
+++ source/bazel/deps/build_files/cereal.BUILD
@@ -3,5 +3,5 @@ package(default_visibility = ["//visibility:public"])
 cc_library(
   name = "main",
   hdrs = glob(["include/**/*.hpp"]),
-  includes = ["include/cereal"],
+  includes = ["include"],
 )
--- source/code/hello_world/serialization/int.cpp
+++ source/code/hello_world/serialization/int.cpp
@@ -3,6 +3,20 @@
 #include <sstream>
 #include <string>
 #include <utility>
+#include "cereal/archives/binary.hpp"
 
 TEST(HelloTest, IntTest) {
+	
+	std::stringstream ss;
+	{
+		cereal::BinaryOutputArchive oarchive(ss);
+		int i = 5;
+		oarchive(i);
+	}
+	{
+		cereal::BinaryInputArchive iarchive(ss);
+		int i = 0;
+		iarchive(i);
+		EXPECT_EQ(i,5);
+	}
 }
