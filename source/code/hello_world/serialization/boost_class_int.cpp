14b0b153f0b72300ae5d669984c3a5319e0c2d7f
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-03-03 13:26:20 -0500

    updates

---
+++ source/code/hello_world/serialization/boost_class_int.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <sstream>
+#include "gtest/gtest.h"
+#define _LIBCPP_ENABLE_CXX17_REMOVED_AUTO_PTR
+#define BOOST_NO_AUTO_PTR
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+struct foo{
+    int i;
+    
+  template <class Archive>
+  void serialize( Archive & ar, const unsigned int version)
+  {
+    ar & i;
+  }
+};
+
+TEST(HelloTest, BoostInt) {
+    
+    std::stringstream ss;
+    {
+        boost::archive::text_oarchive oarchive(ss);
+        foo f;
+        f.i = 5;
+        oarchive << f;
+    }
+    {
+        boost::archive::text_iarchive iarchive(ss);
+        foo f;
+        f.i = 0;
+        iarchive >> f;
+        EXPECT_EQ(f.i,5);
+    }
+}
+
