13d2b6f885b2637c9fc5d1214873d0e3d619d91b
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-03-04 10:23:21 -0500

    updates

--- source/code/ide/main.cpp
+++ source/code/ide/main.cpp
@@ -3,7 +3,117 @@
 #include "code/utilities/graphics/imgui/ui/ui_renders.hpp"
 #include "code/utilities/graphics/imgui/ui/window/window_renderer.hpp"
 
-void each_frame(Window_Settings & f){
+
+
+
+struct position
+{
+    int x = 0;
+    int y = 0;
+};
+
+struct color
+{
+    int r     = 0;
+    int g     = 0;
+    int b     = 0;
+    int alpha = 255;
+    color()   = default;
+    color(int r, int g, int b, int alpha) : r(r), g(g), b(b), alpha(alpha) {}
+};
+
+struct rectangle_settings
+{
+    std::string label;
+    int         width     = 50;
+    int         height    = 50;
+    int         thickness = 1;
+    float       rounding  = 0.0;
+    color       color_border;
+    color       color_fill;
+};
+
+struct mouse_events
+{
+    bool hovered = false;
+    bool clicked = false;
+};
+
+
+
+
+struct ide_settings
+{
+    Sdl_Init_Settings init;
+    Window_Settings method_item;
+};
+
+ImU32 to_imgui_color(const color &c)
+{
+    return IM_COL32(c.r, c.g, c.b, c.alpha);
+}
+
+position window_adjusted_position(position pos)
+{
+    const ImVec2 screen_position = ImGui::GetCursorScreenPos();
+    position     p;
+    p.x = pos.x + screen_position.x;
+    p.y = pos.y + screen_position.y;
+    return p;
+}
+
+bool mouse_clicked_area(position pos, rectangle_settings const &settings)
+{
+    if (ImGui::IsMouseClicked(0))
+    {
+        auto mouse_pos = ImGui::GetMousePos();
+        if (mouse_pos.x > pos.x && mouse_pos.x < pos.x + settings.width)
+        {
+            if (mouse_pos.y >= pos.y && mouse_pos.y <= pos.y + settings.height)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+mouse_events draw_rectangle(ImDrawList *draw_list, position pos, rectangle_settings const &settings)
+{
+    // draw it
+    draw_list->AddRectFilled(ImVec2(pos.x, pos.y),
+                             ImVec2(pos.x + settings.width, pos.y + settings.height),
+                             to_imgui_color(settings.color_fill),
+                             settings.rounding,
+                             ImDrawCornerFlags_All);
+
+
+    draw_list->AddRect(ImVec2(pos.x, pos.y),
+                       ImVec2(pos.x + settings.width, pos.y + settings.height),
+                       to_imgui_color(settings.color_border),
+                       settings.rounding,
+                       ImDrawCornerFlags_All,
+                       settings.thickness);
+
+    // look for any events
+    mouse_events events;
+    if (mouse_clicked_area(pos, settings))
+    {
+        events.clicked = true;
+    }
+    if (ImGui::IsMouseHoveringRect(
+            ImVec2(pos.x, pos.y), ImVec2(pos.x + settings.width - 1, pos.y + settings.height), false))
+    {
+        events.hovered = true;
+    }
+    
+    return events;
+}
+
+
+
+
+void each_frame(ide_settings & settings){
     
     Ui_Renders::render_main_menu_bar([](){
         if (ImGui::MenuItem("hello world", "T"))
@@ -15,7 +125,30 @@ void each_frame(Window_Settings & f){
     bool show_demo_window = true;
     ImGui::ShowDemoWindow(&show_demo_window);
     
-    Window_Renderer::render(f,[&](){
+    
+    Window_Renderer::render(settings.method_item,[&](){
+        
+        position temp;
+        temp.x = -4;
+        temp.y = -5;
+        ImDrawList *draw_list = ImGui::GetWindowDrawList();
+        auto pos = window_adjusted_position(temp);
+        
+        rectangle_settings rec;
+        rec.width     = 6;
+        rec.height    = 6;
+        rec.thickness = 1;
+        rec.rounding  = 0.0;
+        rec.color_border.r = 0;
+        rec.color_border.g = 0;
+        rec.color_border.b = 0;
+        rec.color_fill.r = 50;
+        rec.color_fill.g = 205;
+        rec.color_fill.b = 50;
+        
+        draw_rectangle(draw_list,pos,rec);
+        
+        /*
         ImGuiTreeNodeFlags flags = 0;
         if (ImGui::CollapsingHeader("Implementations", flags)){
             ImGui::Text("%s", "test");
@@ -26,48 +159,56 @@ void each_frame(Window_Settings & f){
         if (ImGui::CollapsingHeader("Benchmarks", flags)){
             ImGui::Text("%s", "test");
         }
+        */
     });
-    
-    
-    
 }
+
+
+ide_settings get_ide_settings(){
+    
+    ide_settings settings;
+    
+    settings.init.main_window.title = "Unilang IDE";
+    
+    settings.method_item.title              = "Function";
+    settings.method_item.menu.title         = "Settings";
+    settings.method_item.menu.shortcut      = "CTRL+F";
+    settings.method_item.position.x         = 20;
+    settings.method_item.position.y         = 38;
+    settings.method_item.position.condition = ImGuiCond_Once;
+    settings.method_item.size.width         = 800;
+    settings.method_item.size.height        = 410;
+    settings.method_item.size.condition     = ImGuiCond_Once;
+    settings.method_item.titlebar           = false;
+    settings.method_item.scrollbar          = true;
+    settings.method_item.has_menu           = false;
+    settings.method_item.move               = true;
+    settings.method_item.resize             = true;
+    settings.method_item.collapse           = true;
+    settings.method_item.nav                = false;
+    settings.method_item.close              = true;
+    settings.method_item.is_open            = false;
+    settings.method_item.focus              = false;
+    settings.method_item.refocus_on_use     = false;
+    settings.method_item.bg_alpha           = 1;
+    
+    return settings;
+}
+
+
 int main()
 {
-
-    Sdl_Init_Settings s;
-    s.main_window.title = "Unilang IDE";
-    Window_Settings f;
+    auto s = get_ide_settings();
     
-    f.title              = "Function";
-    f.menu.title         = "Settings";
-    f.menu.shortcut      = "CTRL+F";
-    f.position.x         = 20;
-    f.position.y         = 38;
-    f.position.condition = ImGuiCond_Once;
-    f.size.width         = 800;
-    f.size.height        = 410;
-    f.size.condition     = ImGuiCond_Once;
-    f.titlebar           = false;
-    f.scrollbar          = true;
-    f.has_menu           = false;
-    f.move               = true;
-    f.resize             = true;
-    f.collapse           = true;
-    f.nav                = false;
-    f.close              = true;
-    f.is_open            = false;
-    f.focus              = false;
-    f.refocus_on_use     = false;
-    f.bg_alpha           = 1;
-    
-    auto before = [](){
+    auto before = [&](){
         
-    ImGuiStyle& style = ImGui::GetStyle();
-    style.WindowBorderSize = 0;
-    style.WindowRounding = 0;
+        //Initial Imgui styling
+        ImGuiStyle& style = ImGui::GetStyle();
+        style.WindowBorderSize = 0;
+        style.WindowRounding = 0;
         
     };
-    return Mechanics::render_each_frame(s,before,[&](){
-        each_frame(f);
+    return Mechanics::render_each_frame(s.init,before,[&](){
+        each_frame(s);
     });
 }
