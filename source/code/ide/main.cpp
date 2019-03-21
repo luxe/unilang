20b1e4379a5aba34ae444c7cfc578e54d154b5ec
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-03-20 23:08:09 -0400

    updates

--- source/code/ide/main.cpp
+++ source/code/ide/main.cpp
@@ -19,7 +19,7 @@ int main()
         //Initial ImGui Mouse settings
         ImGuiIO& io = ImGui::GetIO();
         io.MouseDrawCursor = true;
-        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
         
         
         // io.KeyMap[ImGuiKey_Tab] = SDLK_TAB;                 // Keyboard mapping. ImGui will use those indices to peek into the io.KeyDown[] array.
@@ -33,12 +33,15 @@ int main()
         // io.KeyMap[ImGuiKey_Backspace] = SDLK_BACKSPACE;
         // io.KeyMap[ImGuiKey_Enter] = SDLK_RETURN;
         // io.KeyMap[ImGuiKey_Escape] = SDLK_ESCAPE;
-        // io.KeyMap[ImGuiKey_A] = SDLK_a;
+        //io.KeyMap[ImGuiKey_A] = SDLK_a;
         // io.KeyMap[ImGuiKey_C] = SDLK_c;
         // io.KeyMap[ImGuiKey_V] = SDLK_v;
         // io.KeyMap[ImGuiKey_X] = SDLK_x;
         // io.KeyMap[ImGuiKey_Y] = SDLK_y;
         // io.KeyMap[ImGuiKey_Z] = SDLK_z;
+        
+        
+        //io.KeyCtrl = ((SDL_GetModState() & KMOD_CTRL) != 0);
         
         ImGui::SetMouseCursor(ImGuiMouseCursor_None); //put inside main loop
     
