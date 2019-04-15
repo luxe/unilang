2b20ac828eb54b734b421a144bc14a3930c74899
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-04-14 22:11:37 -0400

    updates

--- source/code/hello_world/protobuf/string_get_set/main.cpp
+++ source/code/hello_world/protobuf/string_get_set/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-#include "code/hello_world/protobuf/hello.pb.h"
+#include "code/hello_world/protobuf/string_get_set/hello.pb.h"
 int main(){
 
 	// I would have just a set "hello world" as the default
 	// in the proto message, but proto3 doesn't support default values like that.
 	// so I'll just test it here with a get and set.
 	// The other problem I noticed, is that when I did try to give a default string value,
-	//the compiler error, was not very helpful at all.  I think it just said that the protobuf compiler or something
-	//just crashed.  It didn't give me line numbers into the proto message at all.
-	//That might be something we try to improve later if needed
+	// the compiler error, was not very helpful at all.  I think it just said that the protobuf compiler or something
+	// just crashed.  It didn't give me line numbers into the proto message at all.
+	// That might be something we try to improve later if needed
 	GOOGLE_PROTOBUF_VERIFY_VERSION;
 	demo::Message message;
 	message.set_foo("hello world");
 	std::cout << message.foo() << std::endl;
 
-  return 0;
+        return 0;
 }
