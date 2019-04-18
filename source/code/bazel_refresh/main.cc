f134ed69ed74f9809429f3764af8d2c0d6fda91b
Trevor Hickey <thickey@uber.com>
2019-04-18 16:59:57 -0400

    updates

--- source/code/bazel_refresh/main.cc
+++ source/code/bazel_refresh/main.cc
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 #include "code/bazel_refresh/bazel_refresher.hpp"
+#include "code/utilities/locale/lib.hpp"
 
 
 int main(int argc, char** argv){
+    Set_English_UTF8_Locale();
     Bazel_Refresher::Refresh();
 }
