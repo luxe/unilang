080af3e1118c6b848efdf2f23c5f23f4e4d0544a
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-05-14 16:47:13 -0400

    updates

--- source/code/utilities/web/dropbox/upload_to_dropbox.cpp
+++ source/code/utilities/web/dropbox/upload_to_dropbox.cpp
@@ -1,10 +1,14 @@
 #include "code/utilities/web/dropbox/dropbox_uploader.hpp"
 #include "code/utilities/secret/secret_getter.hpp"
+#include "code/utilities/web/dropbox/dropbox_upload_settings.hpp"
 #include <iostream>
 
 int main(){
     
-    auto secret = Secret_Getter::Unilang_Dropbox_Access_Key();
-    std::cout << secret << std::endl;
-    Dropbox_Uploader::Upload/*_With_Curl_Command*/("/home/laptop/Desktop/tenor.gif","yup.gif",secret);
+    Dropbox_Upload_Settings settings;
+    settings.access_token = Secret_Getter::Unilang_Dropbox_Access_Key();
+    settings.local_file_path = "/home/laptop/Desktop/tenor.gif";
+    settings.docker_file_path = "yup.gif";
+    settings.strategy = Upload_To_Dropbox_Strategy::CURL_LIB;
+    Dropbox_Uploader::Upload(settings);
 }
