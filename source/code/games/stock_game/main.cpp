3e00a66d77f3aabed2c6b9693bb027ee3fed62d7
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-05-20 16:03:04 -0400

    updates

--- source/code/games/stock_game/main.cpp
+++ source/code/games/stock_game/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "code/games/stock_game/task_executer.hpp"
+#include "code/games/stock_game/deskstock_runner.hpp"
 
 int main(){
-    Task_Executer::Execute_Needed_Tasks();
+    Deskstock_Runner::Run();
 }
