11d420fc270d9bd7e6d305b8b2eb1d025fad70a1
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-05-26 17:48:10 -0400

    updates

--- source/code/deploy/main.cpp
+++ source/code/deploy/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 #include "code/utilities/locale/lib.hpp"
+#include "code/deploy/software_deployer.hpp"
 
 int main(int argc, char** argv){
     Set_English_UTF8_Locale();
     
     //things to export out of the repository
-    //Software_Deployer::Deploy();
+    Software_Deployer::Deploy();
 }
