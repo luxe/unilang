f72a0a7e4f16c14230347610a317009330f8653f
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-06-04 16:47:56 -0400

    updates

--- source/code/utilities/types/strings/creators/lib.hpp
+++ source/code/utilities/types/strings/creators/lib.hpp
@@ -6,3 +6,5 @@ std::string Create_String_Of_N_Chars(unsigned int const& num_of_chars, char cons
 std::string Create_String_Of_N_Spaces(unsigned int num_spaces);
 std::string Char_To_String(unsigned char c);
 std::string Chars_To_String(std::vector<char> chars);
+
+
