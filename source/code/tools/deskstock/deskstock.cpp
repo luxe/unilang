638b99d526c20771fc5cc38019e4d265900601bb
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-06-05 14:17:59 -0400

    updates

--- source/code/tools/deskstock/deskstock.cpp
+++ source/code/tools/deskstock/deskstock.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "code/tools/deskstock/deskstock_runner.hpp"
+#include "code/tools/deskstock/meta_settings.hpp"
 
 int main(){
     
     //should probably do prog args and stuff
-    Deskstock_Runner::Run();
+    
+    Meta_Settings meta;
+    Deskstock_Runner::Run(meta);
 }
