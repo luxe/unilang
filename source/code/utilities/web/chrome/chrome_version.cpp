7b0ae12538d435cba2d6e4dbbf14d0db87a2eb1b
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-06-16 22:43:04 -0400

    updates

---
+++ source/code/utilities/web/chrome/chrome_version.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "code/utilities/web/chrome/chrome_version_getter.hpp"
+
+int main(){
+    auto version = Chrome_Version_Getter::Get_Version();
+    std::cout << version << std::endl;
+}
