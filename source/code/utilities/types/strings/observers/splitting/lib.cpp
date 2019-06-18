62f8a3777565c76e0c4b436eed9e52733ec09bf7
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-06-18 14:46:10 -0400

    updates

--- source/code/utilities/types/strings/observers/splitting/lib.cpp
+++ source/code/utilities/types/strings/observers/splitting/lib.cpp
@@ -21,6 +21,9 @@ std::vector<std::string> Split_Into_Parts_From_Delimiter(std::string const& str,
 std::vector<std::string> Split_Into_Parts_From_Comma(std::string const& str){
 	return Split_Into_Parts_From_Delimiter(str,',');
 }
+std::vector<std::string> Split_Into_Parts_From_Dot(std::string const& str){
+    return Split_Into_Parts_From_Delimiter(str,'.');
+}
  std::vector<std::string> Get_Path_In_Parts(std::string const& str){
  	return Split_Into_Parts_From_Delimiter(str,'/');
 }
