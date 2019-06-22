9c84a2f67a4d52f8cd0be70d4e56e74acf98fe7e
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-06-22 16:14:38 -0400

    updates

---
+++ source/code/utilities/types/strings/observers/padding/lib.cpp
@@ -0,0 +1,9 @@
+#include "code/utilities/types/strings/observers/padding/lib.hpp"
+
+std::string pad_str_to_length(std::string str, size_t length, char c)
+{
+    auto current_length = str.size();
+    for (size_t i = current_length; i < length; ++i){
+    }
+    return str;
+}
