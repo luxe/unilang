6c7ee463284ba46d1ea0d0e153c9822086ae23cb
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-06-27 01:00:43 -0400

    updates

--- source/code/utilities/language_basic/control_structures/branching/lib.hpp
+++ source/code/utilities/language_basic/control_structures/branching/lib.hpp
@@ -16,3 +16,10 @@ bool If_Else(Boolean_Expression b, Function1 f1, Function2 f2){
     if (b){f1();return true;}
     f2(); return false;
 }
+
+template <typename Boolean_Expression, typename Function>
+void Configurable_If(bool enable, Boolean_Expression b, Function f){
+    if (enable){
+        if (b){f();}
+    }
+}
