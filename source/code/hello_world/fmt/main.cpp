6cf31508057998352b01ed993354276b76be69a7
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-07-05 17:30:01 -0400

    updates

--- source/code/hello_world/fmt/main.cpp
+++ source/code/hello_world/fmt/main.cpp
@@ -1,5 +1,9 @@
+#include <iostream>
 #include <fmt/format.h>
 
 int main(){
     fmt::print("Hello, {}!", "world");  // Python-like format string syntax
+    
+    auto x = fmt::format("Hello, {}!","world");
+    std::cout << std::endl << x << std::endl;
 }
