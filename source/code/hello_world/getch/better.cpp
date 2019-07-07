1d6db0982785f6a5c91d980f3068e130c2f90a0d
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-07-07 14:03:42 -0400

    updates

--- source/code/hello_world/getch/better.cpp
+++ source/code/hello_world/getch/better.cpp
@@ -1,6 +1,37 @@
 #include "code/utilities/ncurses/scoped_getch.hpp"
+#include "getch/include/ch.hpp"
+#include <iostream>
 
 int main(){
     
+    
+    t_key keys[] = {
+      {(char *)"[A", K_UP},
+      {(char *)"[B", K_DOWN},
+      {(char *)"[D", K_LEFT},
+      {(char *)"[C", K_RIGHT},
+      {NULL, K_UNK},
+    };
+    
     ScopedGetch getch_guard{};
+    while(true){
+        auto key = ch_get(keys);
+        switch(key){
+            case K_UP:
+            std::cout << "up" << std::endl;
+            break;
+            
+            case K_DOWN:
+            std::cout << "down" << std::endl;
+            break;
+            
+            case K_LEFT:
+            std::cout << "left" << std::endl;
+            break;
+            
+            case K_RIGHT:
+            std::cout << "right" << std::endl;
+            break;
+        }
+    }
 }
