4dfd62432f29b5164b80fb6e7f879cae93993f3e
Trevor Hickey <thickey@uber.com>
2019-07-19 11:30:38 -0400

    updates

--- source/code/hello_world/x11/root_picture/main.cpp
+++ source/code/hello_world/x11/root_picture/main.cpp
@@ -8,109 +8,9 @@
 #include <X11/extensions/shape.h>
 #include <sys/time.h>
 #include <signal.h>
+#include "code/utilities/x11/main_state/x11_main_state_creator.hpp"
 
 
-struct x11_window_geometry {
-  int x = 0;
-  int y = 0;
-  unsigned int width = 0;
-  unsigned int height = 0;
-  unsigned int border_width = 0;
-  unsigned int depth = 0;
-};
-
-struct x11_bg_fg_colors {
-  XColor bg;
-  XColor fg;
-};
-
-struct main_x11_state {
-  
-  //some display/screen info
-  Display * d;
-  int screen;
-  unsigned int depth;
-  
-  //root window info
-  Window root;
-  x11_window_geometry root_geo;
-  
-  //colors
-  x11_bg_fg_colors colors;
-};
-
-
-void set_typical_x11_error_handler(){
-    
-    //error handler
-    XSetErrorHandler([](Display *dpy, XErrorEvent *err){
-            char msg[80];
-            XGetErrorText(dpy, err->error_code, msg, 80);
-            std::cout << "X Error: " << msg  << std::endl;
-            exit(1);
-            return 0;
-    });
-}
-
-Display * open_display(){
-    
-    //create the main display
-    Display *theDisplay = nullptr;
-    if ((theDisplay = XOpenDisplay(NULL)) == NULL) {
-        std::cerr << "can't create display" << std::endl;
-        exit(1);
-    }
-    return theDisplay;
-}
-
-void check_shape_extension(Display * theDisplay){
-
-    //check that we can do shape stuff
-    int event_base = 0;
-    int error_base = 0;
-    auto supported = XShapeQueryExtension(theDisplay,&event_base, &error_base);
-    if (!supported){
-        std::cout << "doesn't support shapes" << std::endl;
-    }
-}
-
-x11_window_geometry get_geometry(Display * theDisplay, Window theRoot){
-    x11_window_geometry geo;
-    Window theTempRoot;
-    XGetGeometry(theDisplay, theRoot, &theTempRoot,
-               &geo.x, &geo.y,
-               &geo.width, &geo.height,
-               &geo.border_width, &geo.depth);
-  return geo;
-}
-
-x11_bg_fg_colors setup_colors(Display * theDisplay, int theScreen, unsigned int theDepth)
-{
-    x11_bg_fg_colors colors;
-    
-    Colormap theColormap = DefaultColormap(theDisplay, theScreen);
-    std::string Foreground = "black";
-    std::string Background = "white";
-    if (theDepth == 1) {
-        Foreground = "black";
-        Background = "white";
-    }
-
-    XColor      theExactColor;
-    if (!XAllocNamedColor(theDisplay, theColormap,
-                Foreground.c_str(), &colors.fg, &theExactColor)) {
-        std::cerr << "Can't XAllocNamedColor" << std::endl;
-        exit(1);
-    }
-
-    if (!XAllocNamedColor(theDisplay, theColormap,
-                Background.c_str(), &colors.bg, &theExactColor)) {
-        std::cerr << "Can't XAllocNamedColor" << std::endl;
-        exit(1);
-    }
-    
-    return colors;
-}
 
 XImage *Load_Xpm_Image(Display * theDisplay, std::string const& file_name){
 
@@ -123,7 +23,7 @@ XImage *Load_Xpm_Image(Display * theDisplay, std::string const& file_name){
       return img;
 }
 
-Pixmap Load_Xbm_Image(main_x11_state const& state, int x, int y, std::string const& file_name){
+Pixmap Load_Xbm_Image(Main_X11_State const& state, int x, int y, std::string const& file_name){
 
     //some garbage boilerplate for loading images
     //we need this pixmap for some reason so we can load other pixmaps?
@@ -201,7 +101,7 @@ void process_x11_events(Display * theDisplay){
     }
 }
 
-GC Create_Graphics_Context(Display * theDisplay, int theScreen, Window theWindow, Window theRoot, x11_bg_fg_colors colors, int width, int height){
+GC Create_Graphics_Context(Display * theDisplay, int theScreen, Window theWindow, Window theRoot, X11_Bg_Fg_Colors colors, int width, int height){
   
     static char garbage[] = {};
     Pixmap BitmapCreatePtr = XCreatePixmapFromBitmapData(theDisplay, theRoot,
@@ -230,59 +130,6 @@ GC Create_Graphics_Context(Display * theDisplay, int theScreen, Window theWindow
     
 }
 
-
-
-struct setup_display_settings {
-    bool syncronize_debug_mode = false;
-    bool set_error_handler = false;
-    bool check_for_shape_extension = true;
-};
-
-
-Display * setup_display(setup_display_settings const& settings){
-  
-    if (settings.set_error_handler){
-        set_typical_x11_error_handler();
-    }
-
-    //create the main display
-    auto d = open_display();
-
-    //turn on debug sync mode
-    if (settings.syncronize_debug_mode) {
-        XSynchronize(d,true);
-    }
-    
-    //feature check
-    if (settings.check_for_shape_extension){
-        check_shape_extension(d);
-    }
-    
-    return d;
-}
-
-
-main_x11_state create_main_x11_state(setup_display_settings const& settings){
-  
-  main_x11_state state;
-  
-    //create the main display
-    state.d = setup_display(settings);
-    
-    //create the screen and depth
-    state.screen = DefaultScreen(state.d);
-    state.depth = DefaultDepth(state.d, state.screen);
-    
-    //create the root window
-    state.root = RootWindow(state.d, state.screen);
-    state.root_geo = get_geometry(state.d, state.root);
-    
-    //colors
-    state.colors = setup_colors(state.d, state.screen, state.depth);
-    
-  return state;
-}
-
 struct x11_image_sprite{
   XImage *main;
   Pixmap bitmap_mask;
@@ -290,7 +137,7 @@ struct x11_image_sprite{
 };
 
 
-x11_image_sprite load_image_sprite(main_x11_state const& state, std::string const& path, std::string const& name){
+x11_image_sprite load_image_sprite(Main_X11_State const& state, std::string const& path, std::string const& name){
   x11_image_sprite sprite;
   sprite.name = name;
   sprite.main = Load_Xpm_Image(state.d, path + name + ".xpm");
@@ -299,7 +146,7 @@ x11_image_sprite load_image_sprite(main_x11_state const& state, std::string cons
 }
 
 
-Window create_game_window(main_x11_state const& state){
+Window create_game_window(Main_X11_State const& state){
   
   //set window attributes
   XSetWindowAttributes  theWindowAttributes;
@@ -324,7 +171,7 @@ Window create_game_window(main_x11_state const& state){
 }
 
 template <typename Fun>
-void x11_game_loop(main_x11_state const& state, Fun fun){
+void x11_game_loop(Main_X11_State const& state, Fun fun){
 
   infinite_interrupt_loop(8000L,[&](){
     
@@ -340,7 +187,7 @@ void x11_game_loop(main_x11_state const& state, Fun fun){
   });
 }
 
-void Draw_Image(main_x11_state const& state, Window theWindow, GC gc, x11_image_sprite const& sprite, int x, int y){
+void Draw_Image(Main_X11_State const& state, Window theWindow, GC gc, x11_image_sprite const& sprite, int x, int y){
   
   int relative_x = 0;
   int relative_y = 0;
@@ -365,11 +212,11 @@ int main(){
     //create the main display
     //this is typical x11 boilerplate setup stuff
     //hopefully customizable enough that you don't have to dig into this
-    setup_display_settings settings;
+    Setup_Display_Settings settings;
     settings.syncronize_debug_mode = false;
     settings.set_error_handler = false;
     settings.check_for_shape_extension = true;
-    auto state = create_main_x11_state(settings);
+    auto state = X11_Main_State_Creator::Create(settings);
     
     //load all the sprites
     //we assume that the file names match what is necessary for rendering xpm and xbm files in x11
@@ -393,8 +240,9 @@ int main(){
     //X11 is notorious for image tearing
     //originally, I had one giant window, and I used PutImage each cycle to put the sprite in a different place.
     //this flickered and looked bad.  So Now I make little windows for each sprite, and place the sprite in the window at 0,0.
-    //instead of moving the images each cycle, we move their window instead.  This looks clean.  I'm not sure if there are any ramifications to this
-    //or if i totally missed something in terms of writing images, but this is the best I can do.
+    //instead of moving the images each cycle, we move the entire window instead.  This looks clean but feels wrong.  I'm not sure if there are any ramifications to this
+    //or if i totally missed something in terms of writing images, but this is the best I can do,
+    //and based on the various complaints I've heard about X this may actually be a correct way to go about this.
     //fwiw, I see everyone on the internet talking about how X11 tears because its old, and its one of the reasons for wayland.
     x11_game_loop(state,[&](){
     
