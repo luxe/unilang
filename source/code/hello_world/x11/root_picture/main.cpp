9b278ac695b2201ffddaf6f8c734690489d3ce97
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-07-19 22:28:42 -0400

    updates

--- source/code/hello_world/x11/root_picture/main.cpp
+++ source/code/hello_world/x11/root_picture/main.cpp
@@ -14,23 +14,23 @@
 #include "code/utilities/x11/sprite/sprite_window_creator.hpp"
 
 
-void Draw_Image(Main_X11_State const& state, Window theWindow, GC gc, Sprite const& sprite, int x, int y){
+void Draw_Image(Main_X11_State const& state, Sprite_Window const& sp){
   
   int relative_x = 0;
   int relative_y = 0;
   
   //position the window instead of the sprite inside the window
   XWindowChanges    theChanges;
-  theChanges.x = x;
-  theChanges.y = y;
-  XConfigureWindow(state.d, theWindow, CWX | CWY, &theChanges);
+  theChanges.x = sp.x;
+  theChanges.y = sp.y;
+  XConfigureWindow(state.d, sp.w, CWX | CWY, &theChanges);
   
-  XShapeCombineMask(state.d, theWindow, ShapeBounding, relative_x , relative_y, sprite.bitmap_mask, ShapeSet);
+  XShapeCombineMask(state.d, sp.w, ShapeBounding, relative_x , relative_y, sp.sprite.bitmap_mask, ShapeSet);
   
        //oneko did this.  I don't think its necessary
        //XFillRectangle(state.d, theWindow, gc, 0, 0, sprite.main->width, sprite.main->height);
   
-  XPutImage(state.d, theWindow, gc, sprite.main, 0,0,relative_x, relative_y,sprite.main->width, sprite.main->height );
+  XPutImage(state.d, sp.w, sp.gc, sp.sprite.main, 0,0,relative_x, relative_y,sp.sprite.main->width, sp.sprite.main->height );
 }
 
 
@@ -57,10 +57,6 @@ int main(){
     
     auto mario = Sprite_Window_Creator::Create(state,mario_stand);
     
-    
-    //auto theWindow = create_sprite_window(state);
-    //auto gc = Create_Graphics_Context(state.d,state.screen,theWindow,state.root,state.colors,mario_stand.main->width, mario_stand.main->height);
-    
     //main looping logic
     //why not just use a timer/sleep?
     //I don't know.  I saw some similar examples using the older
@@ -77,15 +73,12 @@ int main(){
     //and based on the various complaints I've heard about X this may actually be a correct way to go about this.
     //fwiw, I see everyone on the internet talking about how X11 tears because its old, and its one of the reasons for wayland.
     X11_Looper::typical_x11_game_loop(state,[&](){
-    
-    
+
         //draw it
-       static int x_c = 200;
-       static int y_c = 100;
-       ++x_c;
-       ++y_c;
+       ++mario.x;
+       ++mario.y;
       
-      Draw_Image(state,mario.w,mario.gc,mario.sprite,x_c,y_c);
+      Draw_Image(state,mario);
     });
     
 }
