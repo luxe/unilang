247f6b1e627183c294c362ccc24ccd721a532928
Trevor Hickey <thickey@uber.com>
2019-07-30 11:26:04 -0400

    updates

--- source/code/tools/gcalviz/gcalviz.cpp
+++ source/code/tools/gcalviz/gcalviz.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "code/tools/gcalviz/gcalviz_runner.hpp"
+#include "code/tools/gcalviz/meta_settings.hpp"
 
 int main(int const argc, char** const argv){
-    Gcalviz_Runner::Run();
+    
+    Meta_Settings meta;
+    meta.program_name = "gcalviz";
+    Gcalviz_Runner::Run(meta);
 }
