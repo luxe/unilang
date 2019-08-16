b0465e48caca1ae6633a4180e7d60e30b4bff1c1
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-08-15 22:52:45 -0400

    updates

--- source/code/tools/pi/main.cpp
+++ source/code/tools/pi/main.cpp
@@ -3,6 +3,11 @@
 
 int main(){
     mp_exp_t ex = 0;
-    std::cout << Pi_Calculator::Bailey_Borwein_Plouffe_Pi_To_N_Digits(400).get_prec() << std::endl;
-    std::cout << Pi_Calculator::Bailey_Borwein_Plouffe_Pi_To_N_Digits(400).get_str(ex) << std::endl;
+    //std::cout << Pi_Calculator::Bailey_Borwein_Plouffe_Pi_To_N_Digits(400).get_prec() << std::endl;
+    std::cout << Pi_Calculator::Bailey_Borwein_Plouffe_Pi_To_N_Digits(1).get_str(ex)[0] << std::endl;
+    std::cout << Pi_Calculator::Bailey_Borwein_Plouffe_Pi_To_N_Digits(2).get_str(ex)[1] << std::endl;
+    std::cout << Pi_Calculator::Bailey_Borwein_Plouffe_Pi_To_N_Digits(3).get_str(ex)[2] << std::endl;
+    std::cout << Pi_Calculator::Bailey_Borwein_Plouffe_Pi_To_N_Digits(4).get_str(ex)[3] << std::endl;
+    std::cout << Pi_Calculator::Bailey_Borwein_Plouffe_Pi_To_N_Digits(5).get_str(ex)[4] << std::endl;
+    std::cout << Pi_Calculator::Bailey_Borwein_Plouffe_Pi_To_N_Digits(6).get_str(ex)[5] << std::endl;
 }
