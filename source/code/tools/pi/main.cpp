8532011444186bc00de9937f43fa5b3c7569d59c
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-08-15 21:55:22 -0400

    updates

---
+++ source/code/tools/pi/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "code/utilities/math/pi_calculator.hpp"
+
+int main(){
+    mp_exp_t ex = 10;
+    std::cout << Pi_Calculator::Bailey_Borwein_Plouffe_Pi_To_N_Digits(100).get_str(ex) << std::endl;
+}
