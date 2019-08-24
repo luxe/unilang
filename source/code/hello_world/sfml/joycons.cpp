e5df2ea11bb986630a5fc227eeac10acf2954203
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-08-24 15:56:41 -0400

    updates

--- source/code/hello_world/sfml/joycons.cpp
+++ source/code/hello_world/sfml/joycons.cpp
@@ -1,7 +1,8 @@
 #include <string>
 #include <iostream>
 #include <SFML/Window/Joystick.hpp>
-#include "code/utilities/keyboard/joycons/joycon_horizontal_state.hpp"
+#include "code/utilities/keyboard/joycons/joycons.hpp"
+#include "code/utilities/keyboard/joycons/joycon_state_getter.hpp"
 #include "code/utilities/json/functions/lib.hpp"
 
 //jstest-gtk to toy visually
@@ -11,15 +12,12 @@ int main(){
     
     while (true){
         
-        sf::Joystick::update();
+        auto joys = Joycon_State_Getter::Get();
         
-        state.down = sf::Joystick::isButtonPressed(0, 0);
-        state.right = sf::Joystick::isButtonPressed(0, 1);
-        state.left = sf::Joystick::isButtonPressed(0, 2);
-        state.up = sf::Joystick::isButtonPressed(0, 3);
-        state.sl = sf::Joystick::isButtonPressed(0, 4);
-        state.sr = sf::Joystick::isButtonPressed(0, 5);
-        std::cout << As_JSON_String(state) << std::endl;
+        
+        std::cout << As_JSON_String(joys) << std::endl;
+        
+        //std::cout << sf::Joystick::getAxisPosition(0, sf::Joystick::PovX) << std::endl;
         
         
     }
