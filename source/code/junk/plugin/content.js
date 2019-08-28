84fba42491e343e11bd8358ec996f1e529da1703
Trevor Hickey <thickey@uber.com>
2019-08-28 11:48:04 -0400

    updates

--- source/code/junk/plugin/content.js
+++ source/code/junk/plugin/content.js
@@ -1,25 +1,8 @@
-/*
-var elements = document.getElementsByTagName('*');
-var nodes_to_change = []
-var elements_to_change = []
-
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-        if (node.nodeType === 3) {
-            if (node.nodeValue === "engine") {
-                nodes_to_change.push(node)
-                elements_to_change.push(element)
-            }
-        }
-    }
-}
-*/
-
-var found_acronym = {
-    node: {},
-    elem: {},
+function create_found_acronym() {
+    return {
+        node: {},
+        elem: {},
+    };
 };
 
 function find_all_acronyms() {
@@ -29,10 +12,12 @@ function find_all_acronyms() {
         var element = elements[i];
         for (var j = 0; j < element.childNodes.length; j++) {
             var node = element.childNodes[j];
-            if (node.nodeType === 3) {
+            if (node.nodeType === Node.TEXT_NODE) {
                 if (node.nodeValue === "engine") {
+                    var found_acronym = new create_found_acronym();
                     found_acronym.node = node;
                     found_acronym.elem = element;
+                    //elements_to_change.push(element);
                     all_acronyms.push(found_acronym);
                 }
             }
@@ -41,12 +26,12 @@ function find_all_acronyms() {
     return all_acronyms;
 }
 
-var all_acronyms = find_all_acronyms()
+var all_acronyms = find_all_acronyms();
 
 for (var i = 0; i < all_acronyms.length; i++) {
     
     //create outer span to make word a tooltip
-	var outer = document.createElement("span");
+    var outer = document.createElement("span");
     outer.setAttribute('class', 'tooltip');
     
     //insert acronym
@@ -64,4 +49,5 @@ for (var i = 0; i < all_acronyms.length; i++) {
     
     //change the element
     all_acronyms[i].elem.replaceChild(outer, all_acronyms[i].node);
+    //elements_to_change[i].replaceChild(outer, all_acronyms[i].node);
 }
