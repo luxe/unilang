1ecd0879ca3c10c62ce8dccecc473d3eb141e633
Trevor Hickey <thickey@uber.com>
2019-08-28 11:25:40 -0400

    updates

--- source/code/junk/plugin/content.js
+++ source/code/junk/plugin/content.js
@@ -1,3 +1,4 @@
+/*
 var elements = document.getElementsByTagName('*');
 var nodes_to_change = []
 var elements_to_change = []
@@ -10,23 +11,57 @@ for (var i = 0; i < elements.length; i++) {
             if (node.nodeValue === "engine") {
                 nodes_to_change.push(node)
                 elements_to_change.push(element)
-                //var toolTipDiv = document.createElement("strong");
-                //var word_inside = document.createTextNode(node.nodeValue);
-                //toolTipDiv.appendChild(word_inside);
-                //element.replaceChild(toolTipDiv, node);
             }
         }
     }
 }
+*/
 
-for (var i = 0; i < nodes_to_change.length; i++) {
-	var toolTipDiv = document.createElement("u");
-        toolTipDiv.setAttribute('class', 'tooltip');
-        var word_inside = document.createTextNode(nodes_to_change[i].nodeValue);
-        toolTipDiv.appendChild(word_inside);
-        elements_to_change[i].replaceChild(toolTipDiv, nodes_to_change[i]);
+var found_acronym = {
+    node: {},
+    elem: {},
+};
+
+function find_all_acronyms() {
+    all_acronyms = []
+    var elements = document.getElementsByTagName('*');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        for (var j = 0; j < element.childNodes.length; j++) {
+            var node = element.childNodes[j];
+            if (node.nodeType === 3) {
+                if (node.nodeValue === "engine") {
+                    found_acronym.node = node;
+                    found_acronym.elem = element;
+                    all_acronyms.push(found_acronym);
+                }
+            }
+        }
+    }
+    return all_acronyms;
 }
 
+var all_acronyms = find_all_acronyms()
 
-//console.log(nodes_to_change.length)
-//alert(nodes_to_change.length)
+for (var i = 0; i < all_acronyms.length; i++) {
+    
+    //create outer span to make word a tooltip
+	var outer = document.createElement("span");
+    outer.setAttribute('class', 'tooltip');
+    
+    //insert acronym
+    var word = document.createTextNode(all_acronyms[i].node.nodeValue);
+    outer.appendChild(word);
+    
+    //create inner span to make tooltip text
+    var inner = document.createElement("span");
+    inner.setAttribute('class', 'tooltiptext');
+    
+    //insert definition
+    var defined = document.createTextNode("hello world");
+    inner.appendChild(defined);
+    outer.appendChild(inner);
+    
+    //change the element
+    all_acronyms[i].elem.replaceChild(outer, all_acronyms[i].node);
+}
