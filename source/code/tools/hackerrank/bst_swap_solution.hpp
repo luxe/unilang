7865a62686ca4bc3988cd51b838a60e2e0869dc4
Trevor Hickey <thickey@uber.com>
2019-09-04 11:56:56 -0400

    updates

--- source/code/tools/hackerrank/bst_swap_solution.hpp
+++ source/code/tools/hackerrank/bst_swap_solution.hpp
@@ -22,21 +22,62 @@ bool is_parent_of(T* parent, T* child){
     return false;
 }
 
+template <typename T>
+bool are_adjacent(T* node1, T* node2){
+    return is_parent_of(node1,node2) || is_parent_of(node2,node1);
+}
+
+template <typename T>
+T* get_parent(T* node1, T* node2){
+    if (is_parent_of(node1,node2)){
+        return node1;
+    }
+    if (is_parent_of(node2,node1)){
+        return node2;
+    }
+    return nullptr;
+}
+
+
+template <typename T>
+bool left_child_breaks_constraint(T* parent)
+{
+    if (parent->left){
+        if (parent->left->val > parent->val){
+            return true;
+        }
+    }
+    return false;
+}
+
+template <typename T>
+bool right_child_breaks_constraint(T* parent)
+{
+    if (parent->right){
+        if (parent->right->val < parent->val){
+            return true;
+        }
+    }
+    return false;
+}
+
 //given a parent node,
 //return the immediate child that invalidates a BST structure
 template <typename T>
 T* find_invalid_child(T* parent){
-    if (parent->left){
-        if (parent->left->val > parent->val){
-            return parent->left;
-        }
+    
+    if (left_child_breaks_constraint(parent)){
+        return parent->left;
     }
-    if (parent->right){
-        if (parent->right->val < parent->val){
-            return parent->right;
-        }
+    if (right_child_breaks_constraint(parent)){
+        return parent->right;
     }
     return nullptr;
+}
+
+template <typename T>
+bool both_children_are_invalid(T* parent){
+    return left_child_breaks_constraint(parent) && right_child_breaks_constraint(parent);
 }
 
 //algorithm specific
@@ -44,8 +85,8 @@ T* find_invalid_child(T* parent){
 template <typename T>
 T* choose_first_node(T* previous, T* t){
     
-    if (is_parent_of(t,previous)){
-        return t;
+    if (are_adjacent(t,previous)){
+        return get_parent(t,previous);
     }
     return previous;
 }
@@ -67,12 +108,18 @@ void swap_found_nodes(std::pair<T*,T*> & finds){
         return;
     }
     
+    std::cout << finds.first->val << " " << finds.second->val << std::endl;
+    
     //only one node was stored
     //there is a problem with adjacent nodes
     //luckily we stored the parent, and can
     //rediscover which node needs swapped
+    if (both_children_are_invalid(finds.first)){
+        std::cout << "this" << std::endl;
+        std::swap(finds.first->left->val,finds.first->right->val);
+        return;
+    }
     if (!finds.second){
-        
         auto child = find_invalid_child(finds.first);
         std::swap(finds.first->val,child->val);
         return;
@@ -80,7 +127,15 @@ void swap_found_nodes(std::pair<T*,T*> & finds){
     
     //non-adjacent nodes need swapped,
     //and they are stored correctly.
-    std::swap(finds.first->val,finds.second->val);
+    auto child1 = find_invalid_child(finds.first);
+    auto child2 = find_invalid_child(finds.second);
+    
+    auto to_swap1 = child1 ? child1 : finds.first;
+    auto to_swap2 = child2 ? child2 : finds.second;
+    
+    std::cout << "actual: " << to_swap1->val << " " << to_swap2->val << std::endl;
+    
+    std::swap(to_swap1->val,to_swap2->val);
 }
 
 template <typename T>
@@ -88,14 +143,19 @@ void store_nodes_for_swapping(std::pair<T*,T*> & finds, T* previous, T* t){
     if (previous->val > t->val){
         
         
-        //handle storing first node
+        //handle storing first discrepancy
         if (!finds.first){
             finds.first = choose_first_node(previous,t);
         }
         
-        //handle storing second node
+        //handle storing non-first discrepancies
         else{
-            finds.second = choose_second_node(previous,t);
+            
+            if (finds.second){
+                finds.first = finds.second;
+            }
+            
+            finds.second = choose_first_node(previous,t);
         }
     }
 }
--- source/code/tools/hackerrank/bst_swap_test.cpp
+++ source/code/tools/hackerrank/bst_swap_test.cpp
@@ -3,12 +3,26 @@
 #include "gtest/gtest.h"
 #include <algorithm>
 
+void wrong_by_only_two_nodes(std::vector<int> nodes){
+  auto sorted = nodes;
+  std::sort(sorted.begin(),sorted.end());
+  
+  int discrepency_count = 0;
+  for (size_t i = 0; i < sorted.size(); ++i){
+    if (nodes[i] != sorted[i]){
+      ++discrepency_count;
+    }
+  }
+  EXPECT_EQ(discrepency_count,2);
+}
+
 void check_tree_fix(BinaryNode<int> & root){
   auto before = Tree_Traversal::inorder_traversal_as_list(&root);
   fix_bst(&root);
   auto after = Tree_Traversal::inorder_traversal_as_list(&root);
   
   EXPECT_FALSE(std::is_sorted(before.begin(),before.end()));
+  wrong_by_only_two_nodes(before);
   EXPECT_TRUE(std::is_sorted(after.begin(),after.end()));
   
 }
@@ -59,6 +73,41 @@ TEST(BST_Fix, NonAdjacent_2) {
         check_tree_fix(root);
 }
 
+TEST(BST_Fix, NonAdjacent_3) {
+        /*
+               2
+             /  \
+            3    1
+        3 and 1 are swapped (non-adjacent)
+        */
+        BinaryNode<int> root(2);
+        BinaryNode<int> r1(3);
+        BinaryNode<int> r2(1);
+        root.left = &r1;
+        root.right = &r2;
+        check_tree_fix(root);
+}
+TEST(BST_Fix, NonAdjacent_4) {
+        /*
+               2
+             /  \
+            1    3
+           /      \
+          4        0
+        4 and 0 are swapped (non-adjacent)
+        */
+        BinaryNode<int> root(2);
+        BinaryNode<int> r1(1);
+        BinaryNode<int> r2(3);
+        BinaryNode<int> r3(4);
+        BinaryNode<int> r4(0);
+        root.left = &r1;
+        root.right = &r2;
+        root.left->left = &r3;
+        root.right->right = &r4;
+        check_tree_fix(root);
+}
+
 TEST(BST_Fix, Adjacent_1) {
         /*
               10
@@ -66,7 +115,7 @@ TEST(BST_Fix, Adjacent_1) {
             6    20
            / \
           2   5
-        5 and 6 are swapped(non-adjacent)
+        5 and 6 are swapped(adjacent)
         */
         BinaryNode<int> root(10);
         BinaryNode<int> r1(6);
@@ -87,12 +136,33 @@ TEST(BST_Fix, Adjacent_2) {
             4    20
            / \
           5   7
-        4 and 5 are swapped(non-adjacent)
+        4 and 5 are swapped(adjacent)
         */
         BinaryNode<int> root(10);
         BinaryNode<int> r1(4);
         BinaryNode<int> r2(20);
         BinaryNode<int> r3(5);
+        BinaryNode<int> r4(7);
+        root.left = &r1;
+        root.right = &r2;
+        root.left->left = &r3;
+        root.left->right = &r4;
+        check_tree_fix(root);
+}
+
+TEST(BST_Fix, Adjacent_3) {
+        /*
+               5
+             /  \
+            10    20
+           / \
+          4   7
+        5 and 10 are swapped(adjacent)
+        */
+        BinaryNode<int> root(5);
+        BinaryNode<int> r1(10);
+        BinaryNode<int> r2(20);
+        BinaryNode<int> r3(4);
         BinaryNode<int> r4(7);
         root.left = &r1;
         root.right = &r2;
