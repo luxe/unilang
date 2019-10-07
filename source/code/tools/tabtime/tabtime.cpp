465827ab49b990c01da0ffeea35dc1f82fd1c2a5
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-10-07 00:26:32 -0400

    updates

--- source/code/tools/tabtime/tabtime.cpp
+++ source/code/tools/tabtime/tabtime.cpp
@@ -1,16 +1,6 @@
-#include <iostream>
-#include <cstdlib>
-#include <thread>
-#include "code/utilities/shell/lib.hpp"
+#include "code/tools/tabtime/tabtime_runner.hpp"
 
 int main(int const argc, char** const argv){
     
-    std::cout << "hello world" << std::endl;
-    
-    //start the websocket server
-    std::thread t([&](){
-        execute("/usr/local/bin/chromix-too-server");
-    });
-    
-    t.join();
+    Tabtime_Runner::Run();
 }
