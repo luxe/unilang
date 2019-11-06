197e2f67cdb633b0022bb1d60da19991d5685af5
Trevor Hickey <thickey@uber.com>
2019-11-06 10:24:14 -0500

    updates

--- source/code/hello_world/variant/main.cpp
+++ source/code/hello_world/variant/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include "code/hello_world/variant/root.hpp"
+#include "code/utilities/types/variant/lib.hpp"
 
 int main(){
+    
+    //create A
+    A a;
+    a.str = "test";
+    
+    //create B
+    B b;
+    b.num = 123;
+    
+    //create Root
+    Root r;
+    
+    r.types = a;
+    std::visit(overloaded {
+        [&](A const& x) {std::cout << x.str << std::endl;},
+        [&](B const& x) {std::cout << x.num << std::endl;},
+    }, r.types);
+    
+    r.types = b;
+    std::visit(overloaded {
+        [&](A const& x) {std::cout << x.str << std::endl;},
+        [&](B const& x) {std::cout << x.num << std::endl;},
+    }, r.types);
+    
 
 }
