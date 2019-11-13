69badd27ebe7cc553bddbfe7c369fcba73e570da
Trevor Hickey <thickey@uber.com>
2019-11-13 12:52:01 -0500

    updates

--- source/code/tools/external_deps_analysis/main.cpp
+++ source/code/tools/external_deps_analysis/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include "code/tools/external_deps_analysis/task_executer.hpp"
+#include "code/tools/external_deps_analysis/program_options/program_options.hpp"
 
-int main(){
-    Task_Executer::Execute_Needed_Tasks();
+
+int main(int const argc, char** const argv){
+    
+    Program_Options program_options(argc, argv);
+    Task_Executer::Execute_Needed_Tasks(program_options);
 }
+
