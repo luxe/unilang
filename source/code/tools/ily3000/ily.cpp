5d7eeeecd196162f6cf2b6c9e92f031ffcfea929
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-11-16 00:29:02 -0500

    updates

--- source/code/tools/ily3000/ily.cpp
+++ source/code/tools/ily3000/ily.cpp
@@ -8,11 +8,11 @@
 #include <SFML/Graphics/Transformable.hpp>
 #include "code/utilities/keyboard/joycons/joycon_state_getter.hpp"
 #include "code/utilities/json/functions/lib.hpp"
-#include "code/tools/math_game/assets/assets_loader.hpp"
-#include "code/tools/math_game/state/game_state.hpp"
-#include "code/tools/math_game/state/game_state_getter.hpp"
-#include "code/tools/math_game/core/frame_renderer.hpp"
-#include "code/tools/math_game/core/state_updater.hpp"
+#include "code/tools/ily3000/assets/assets_loader.hpp"
+#include "code/tools/ily3000/state/game_state.hpp"
+#include "code/tools/ily3000/state/game_state_getter.hpp"
+#include "code/tools/ily3000/core/frame_renderer.hpp"
+#include "code/tools/ily3000/core/state_updater.hpp"
 #include "code/utilities/graphics/sfml/game_loop.hpp"
 #include "code/utilities/graphics/sfml/standard_sfml_game_loop.hpp"
 
@@ -24,7 +24,7 @@ int main()
     auto state = Game_State_Getter::Get();
     
     Standard_Sfml_Game_Loop::Run(
-    "Math Game",
+    "ILY 3000",
     [&](sf::RenderWindow & window, sf::Time const& TimePerFrame){
         State_Updater::Run_Frame_Logic(window,TimePerFrame,state,assets);
     },
