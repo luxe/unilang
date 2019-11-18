4e14a3ba144f69794ab14b67971b003e21e61bb3
Trevor Hickey <thickey@uber.com>
2019-11-18 16:34:16 -0500

    updates

--- source/code/tools/sha256sum_url/sha256sum_url.cpp
+++ source/code/tools/sha256sum_url/sha256sum_url.cpp
@@ -4,6 +4,7 @@
 
 int main(int const argc, char** const argv){
     
+    //this can be done with a simple curl and sha256 command
     auto sha = Sha256sum_Of_Url_Artifact(argv[1]);
     std::cout << sha << std::endl;
 }
