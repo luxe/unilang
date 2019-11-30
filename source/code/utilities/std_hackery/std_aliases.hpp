4ecefdd4ec2199572cabc49a16be8388c4b26dd4
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-11-30 00:07:06 -0500

    updates

--- source/code/utilities/std_hackery/std_aliases.hpp
+++ source/code/utilities/std_hackery/std_aliases.hpp
@@ -3,5 +3,6 @@
 #include <string>
 
 using string_grid = std::vector<std::vector<std::string>>;
+using string_grids = std::vector<std::vector<std::vector<std::string>>>;
 using strings = std::vector<std::string>;
 using words = std::vector<std::string>;
