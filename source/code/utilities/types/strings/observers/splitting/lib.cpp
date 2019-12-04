642e44f451f130bc28156c76681fe9dd3bae85de
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-12-04 00:39:13 -0500

    updates

--- source/code/utilities/types/strings/observers/splitting/lib.cpp
+++ source/code/utilities/types/strings/observers/splitting/lib.cpp
@@ -34,6 +34,9 @@ std::vector<std::string> Split_By_Spaces(std::string const& str){
 std::vector<std::string> Split_Into_Parts_From_Newline(std::string const& str){
     return Split_Into_Parts_From_Delimiter(str,'\n');
 }
+std::vector<std::string> Split_Into_Parts_From_Dash(std::string const& str){
+    return Split_Into_Parts_From_Delimiter(str,'-');
+}
 std::string Get_Path_After_Desktop(std::string const& str)
 {
 	auto parts = Get_Path_In_Parts(str);
