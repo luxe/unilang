0efd1bfd0fd90d11400ea6c297e9d7184fa08e22
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-12-19 00:44:59 -0500

    updates

--- source/code/utilities/filesystem/files/traverse/lib.hpp
+++ source/code/utilities/filesystem/files/traverse/lib.hpp
@@ -1,1 +1,16 @@
 #pragma once
+
+Recurse_Each_Regular_File
+
+for(auto iterEntry = fs::recursive_directory_iterator(pathToShow); iterEntry != fs::recursive_directory_iterator(); ++iterEntry ) {
+    const auto filenameStr = iterEntry->path().filename().string();
+    std::cout << std::setw(iterEntry.depth()*3) << "";
+    if (iterEntry->is_directory()) {
+        std::cout << "dir:  " << filenameStr << '\n';
+    }
+    else if (iterEntry->is_regular_file()) {
+        std::cout << "file: " << filenameStr << '\n';
+    }
+    else
+        std::cout << "??    " << filenameStr << '\n';
+}
