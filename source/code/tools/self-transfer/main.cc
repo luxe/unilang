93268be20d4021ec6cf606e36418c29fd7ae29f3
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-12-22 18:23:04 -0500

    updates

--- source/code/tools/self-transfer/main.cc
+++ source/code/tools/self-transfer/main.cc
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
-#include "code/self-transfer/repo_transferer.hpp"
+#include "code/tools/self-transfer/repo_transferer.hpp"
 
 
 //this was an attempt to transfer one repo (from one machine), into the repo on another machine
-//due to incompatibilities between the repos, the ideas was abandoned
+//due to incompatibilities between the repos, the idea was abandoned
 //my desire was later met by getting bazel to work locally on the destination machine (outside the repo's version of bazel)
 int main(int argc, char** argv){
     Repo_Transferer::Transfer();
