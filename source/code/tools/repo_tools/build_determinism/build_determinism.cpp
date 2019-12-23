f5e060086ceb34166bdaafccbae52151ea16c307
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-12-22 23:18:28 -0500

    updates

---
+++ source/code/tools/repo_tools/build_determinism/build_determinism.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "code/tools/repo_tools/build_determinism/task_executer.hpp"
+#include "code/tools/repo_tools/build_determinism/program_options/program_options.hpp"
+
+
+int main(int const argc, char** const argv){
+    
+    Program_Options program_options(argc, argv);
+    Task_Executer::Execute_Needed_Tasks(program_options);
+}
+
