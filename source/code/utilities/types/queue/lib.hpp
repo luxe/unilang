3081b38598da333ff3a9e0b9e56de500b08b6b7e
Trevor Hickey <thickey@uber.com>
2019-12-23 10:46:50 -0500

    updates

--- source/code/utilities/types/queue/lib.hpp
+++ source/code/utilities/types/queue/lib.hpp
@@ -1,9 +1,26 @@
 #pragma once
 #include <queue>
+#include <set>
+#include <unordered_set>
+#include "code/utilities/types/sets/lib.hpp"
 
 template <typename T>
 T Pop_Front(std::queue<T> & q){
     T x = q.front();
     q.pop();
     return x;
+}
+
+template <typename T, typename Fun>
+void Observe_Each_Item_In_Queue_Avoiding_Duplicates(std::queue<T> & q, Fun fun){
+    
+    std::set<T> seen;
+    while (!q.empty()){
+        auto next = Pop_Front(q);
+        if (!Exists_In_Set(seen,next)){
+            seen.insert(next);
+            fun(q,next);
+        }
+    }
+    
 }
