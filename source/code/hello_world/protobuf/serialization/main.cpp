59ab336b8737aaad81e60372f660441ee07ddb79
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-12-23 23:34:48 -0500

    updates

--- source/code/hello_world/protobuf/serialization/main.cpp
+++ source/code/hello_world/protobuf/serialization/main.cpp
@@ -1,10 +1,15 @@
-
 #include <iostream>
+#include <fstream>
 #include "code/hello_world/protobuf/serialization/message.pb.h"
 int main(){
 
+    //construct
     GOOGLE_PROTOBUF_VERIFY_VERSION;
     demo::Message message;
     message.set_foo("hello world");
+    
+    //serialize
+    std::ofstream ofs("/home/laptop/Desktop/sensor.data", std::ios_base::out | std::ios_base::binary);
+    message.SerializeToOstream(&ofs);
     return 0;
 }
