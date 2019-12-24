7a6913aedbf7c142ff53afdbb46473ad5bf7193f
Trevor Hickey <TrevorJamesHickey@gmail.com>
2019-12-24 15:01:20 -0500

    updates

--- source/code/hello_world/protobuf/serialization/main.cpp
+++ source/code/hello_world/protobuf/serialization/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include "code/hello_world/protobuf/serialization/message.pb.h"
+
+
+template <typename T>
+void Serialize(T const& t, std::string const& path){
+    std::ofstream ofs(path, std::ios_base::out | std::ios_base::binary);
+    bool was_successful = t.SerializeToOstream(&ofs);
+    if (!was_successful){
+        std::cerr << "failed to serialize" << std::endl;
+    }
+}
+
+template <typename T>
+T Deserialize(std::string const& path){
+    T t;
+    std::ifstream ifs(path, std::ios_base::binary);
+    bool was_successful = t.ParseFromIstream(&ifs);
+    if (!was_successful){
+        std::cerr << "failed to deserialize" << std::endl;
+    }
+    return t;
+    
+}
+
 int main(){
 
     //construct
@@ -9,8 +32,11 @@ int main(){
     message.set_foo("hello world");
     
     //serialize
-    std::ofstream ofs("/home/laptop/Desktop/message.serialized", std::ios_base::out | std::ios_base::binary);
-    message.SerializeToOstream(&ofs);
+     Serialize(message,"/home/laptop/Desktop/message.serialized");
+     auto t = Deserialize<demo::Message>("/home/laptop/Desktop/message.serialized");
+     std::cout << t.foo() << std::endl;
+    
+    //deserialze
     
     
     return 0;
