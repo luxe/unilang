122b3a7b51d245018a31ff26a65c40b50d82690b
Trevor Hickey <thickey@uber.com>
2020-01-14 17:03:13 -0500

    updates

--- source/code/utilities/processes/lib.cpp
+++ source/code/utilities/processes/lib.cpp
@@ -4,3 +4,6 @@
 bool Process_Is_Running(std::string const& process_name){
   return Successful_Run_Of_Command("pgrep " + process_name);
 }
+std::string Pid_Of(std::string const& process_name){
+    return execute("pidof " + process_name);
+}
--- source/code/utilities/processes/lib.hpp
+++ source/code/utilities/processes/lib.hpp
@@ -2,4 +2,5 @@
 #include <string>
 
 bool Process_Is_Running(std::string const& process_name);
+std::string Pid_Of(std::string const& process_name);
 
