1f35bc17f522c04c4a6c20926c32599e4eba5aa4
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-02-04 22:16:07 -0500

    updates

---
+++ source/code/hello_world/sfml/f310_gamepads.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include <SFML/Window/Joystick.hpp>
+#include "code/utilities/keyboard/F310_gamepad/functions/f310_gamepads_state_getter.hpp"
+#include "code/utilities/json/functions/lib.hpp"
+
+int main()
+{
+    while (true){
+        auto controllers = F310_Gamepads_State_Getter::Get();
+        std::cout << As_JSON_String(controllers) << std::endl;
+    }
+
+    return 0;
+}
