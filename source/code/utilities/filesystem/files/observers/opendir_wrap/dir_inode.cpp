d6a40cfffd9f315e26b6284fed2cc48b065e46ef
Trevor Hickey <thickey@uber.com>
2020-02-18 12:46:09 -0500

    updates

---
+++ source/code/utilities/filesystem/files/observers/opendir_wrap/dir_inode.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <iomanip>
+#include "code/utilities/filesystem/files/observers/opendir_wrap/opendir_wrap.hpp"
+
+int main(int argc, char *argv[]){
+    std::cout << std::boolalpha;
+    std::cout << Opendir_Wrap::Get_Inode(argv[1]) << std::endl;
+}
