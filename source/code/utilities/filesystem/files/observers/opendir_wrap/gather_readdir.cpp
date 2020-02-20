a1448a50d7ab4ac660d4e3d300850f5934e94140
Trevor Hickey <thickey@uber.com>
2020-02-20 10:50:03 -0500

    updates

--- source/code/utilities/filesystem/files/observers/opendir_wrap/gather_readdir.cpp
+++ source/code/utilities/filesystem/files/observers/opendir_wrap/gather_readdir.cpp
@@ -6,6 +6,6 @@ int main(int argc, char *argv[]){
     std::cout << std::boolalpha;
     
     std::vector<std::string> dirs;
-    auto m = Opendir_Wrap::Gather_Readdir_Seperate_Dirs(argv[1],dirs);
+    auto m = Opendir_Wrap::Gather_Readdir_Seperate_Dirs2(argv[1],dirs);
     std::cout << m.size() << " " << dirs.size() << std::endl;
 }
