321d64798f5a83f4daedcf04914e008f051251e3
Trevor Hickey <thickey@uber.com>
2020-02-28 11:26:56 -0500

    updates

--- source/code/utilities/filesystem/files/creating/lib.cpp
+++ source/code/utilities/filesystem/files/creating/lib.cpp
@@ -7,13 +7,13 @@ void Create_File_Even_If_The_Path_Doesnt_Exist(std::string const& path_to_file){
   auto path = Get_File_Path_Without_The_Filename(path_to_file);
   Create_Path_If_It_Doesnt_Already_Exist(path);
 }
-void Write_To_File(std::string path_to_file, std::string content)
+void Write_To_File(std::string path_to_file, std::string const& content)
 {
 	std::ofstream outfile(path_to_file);
 	outfile << content;
     outfile.close();
 }
-void Write_To_File(std::string path_to_file, std::wstring content)
+void Write_To_File(std::string path_to_file, std::wstring const& content)
 {
 	std::wofstream outfile(path_to_file);
 	outfile << content;
--- source/code/utilities/filesystem/files/creating/lib.hpp
+++ source/code/utilities/filesystem/files/creating/lib.hpp
@@ -2,5 +2,5 @@
 #include <string>
 
 void Create_File_Even_If_The_Path_Doesnt_Exist(std::string const& path_to_file);
-void Write_To_File(std::string path_to_file, std::string content);
-void Write_To_File(std::string path_to_file, std::wstring content);
+void Write_To_File(std::string path_to_file, std::string const& content);
+void Write_To_File(std::string path_to_file, std::wstring const& content);
