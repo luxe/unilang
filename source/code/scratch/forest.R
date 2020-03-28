d09d4b7a699230d2d78db4c9a8e8b4f19e4bc5e8
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-03-27 23:58:31 -0400

    updates

--- source/code/scratch/forest.R
+++ source/code/scratch/forest.R
@@ -16,7 +16,7 @@ mytheme <- function(base_size = 12, base_family = "sans"){
     strip.text = element_text(face = "bold", size = 12, color = "white"),
     legend.position = " ",
     legend.justification = "top", 
-    panel.border = element_rect(color = "grey", fill = NA, size = 1)
+    panel.border = element_rect(color = "grey", fill = NA, size = .5)
   )
 }
 
@@ -28,18 +28,22 @@ Upper = c(0.75, 0.64, 1.91, 0.62, 2.03, 0.85, 0.77, 0.72, 1.84, 0.67, 2.02, 0.81
 
 dat = data.frame(Parameter, OR, Lower, Upper)
 
+limit_line_height=.5
+limit_line_thickness=1
+point_estimate_thickness=.5
+
 dat$Parameter = factor(dat$Parameter, levels=c("Preterm birth", "SGA", "LGA", "LBW", "Macrosomia", "Low Apgar")) 
 g = ggplot(data=dat, aes(x=Race, y=OR, ymin=Lower, ymax=Upper, color=Parameter))
-g = g + geom_pointrange(aes(col=Race), lwd=0.8)  
+g = g + geom_pointrange(aes(col=Race), lwd=point_estimate_thickness)  
 g = g + geom_hline(aes(fill=Race), yintercept =1, linetype=2)
 g = g + xlab("") + ylab("Odds ratios of birth outcomes among excessive weight gain stratified by race: normal weight gain as reference group")
-g = g + geom_errorbar(aes(ymin=Lower, ymax=Upper, col=Race), width=0.5, cex=1)
+g = g + geom_errorbar(aes(ymin=Lower, ymax=Upper, col=Race), width=limit_line_height, cex=limit_line_thickness)
 g = g + facet_wrap(~Parameter, strip.position="left", nrow=9, scales = "free_y")
 g = g + theme(plot.title=element_text(size=16,face="bold"),
         axis.text.y=element_blank(),
         axis.ticks.y=element_blank(),
         axis.text.x=element_text(face="bold"),
-        axis.title=element_text(size=12,face="bold"),
+        axis.title=element_text(size=10,face="bold"),
         strip.text.y = element_text(hjust=0,vjust = 1,angle=180,face="bold"))
 g + coord_flip() + theme_bw() + theme(legend.position="none")
 
