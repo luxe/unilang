5833415316c7a0c7dfb8fe5fa4f4670a1e252618
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-05-09 22:04:24 -0400

    updates

---
+++ source/code/utilities/data_structures/unique_max_counter/unique_max_counter.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <unordered_map>
+#include <map>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <utility>
+#include <functional>
+
+
+//TODO: turn into global functions
+template <typename T>
+class Unique_Counter{
+  
+  public:
+  //thing to count, current amount, max
+  std::map<T,std::pair<int,int>> hash_table;
+  
+  void Add(T const& t, int max){
+    auto search = hash_table.find(t);
+    if(search != hash_table.end()) {
+      search->second.first++;
+    }
+    else{
+      hash_table.emplace(t,std::pair<int,int>{1,max});
+    }
+  }
+  
+  bool At_Max(T const& t){
+    auto search = hash_table.find(t);
+    if(search != hash_table.end()) {
+      if (search->second.first == search->second.second){
+        return true;
+      }
+    }
+    return false;
+  }
+  
+};
