ed68e9e957f93ed0e9dd9deb57208276a404f965
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-05-15 16:03:09 -0400

    test for tree serialization

--- source/code/hello_world/serialization/tree/nonintrusive.cpp
+++ source/code/hello_world/serialization/tree/nonintrusive.cpp
@@ -19,8 +19,6 @@ void from_json(const nlohmann::json& j, foo& f) {
     j.at("i").get_to(f.i);
 }
 
-
-
 tree<foo> Build_Tree() {
     tree<foo> tr;
     auto top = tr.begin();
@@ -38,15 +36,39 @@ tree<foo> Build_Tree() {
     auto f5=tr.append_child(f4,f);
     f.i = 6;
     auto f6=tr.append_child(f5,f);
+    f.i = 7;
+    auto f7=tr.insert(top,f);
+    f.i = 8;
+    auto f8=tr.insert(f7,f);
     
     return tr;
+}
+
+template <typename T>
+std::pair<std::string,std::string> json_serialize_deserialize_serialize(T obj){
+    
+    std::pair<std::string,std::string> results;
+    
+    //serialize
+    nlohmann::json j = obj;
+    results.first = j.dump();
+    
+    //deserialize
+    T obj2 = j.get<T>();
+    
+    //serialize
+    nlohmann::json j2 = obj2;
+    results.second = j2.dump();
+    
+    return results;
 }
 
 TEST(Serialization, TreeNonIntrusive) {
     
     auto tr = Build_Tree();
-    nlohmann::json j = tr;
-    std::cout << j.dump() << std::endl;
-    auto f2 = j.get<tree<foo>>();
-    //EXPECT_EQ(f.i,f2.i);
+    
+    auto results = json_serialize_deserialize_serialize(tr);
+    //std::cout << results.first << std::endl;
+    //std::cout << results.second << std::endl;
+    EXPECT_EQ(results.first,results.second);
 }
