e1f20a4f9c74e18bb83c210d2d66115b07588be2
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-05-19 16:05:23 -0400

    updates

--- source/code/hello_world/audioFile/main.cpp
+++ source/code/hello_world/audioFile/main.cpp
@@ -1,5 +1,19 @@
 #include "AudioFile.h"
+#include "code/utilities/types/vectors/operators/lib.hpp"
+#include "code/utilities/peripheral/sound/wav/wav_file_join_settings.hpp"
+#include "code/utilities/peripheral/sound/wav/wav_file_joiner.hpp"
 
 int main(){
-    AudioFile<double> audioFile;
+    
+    
+    Wav_File_Join_Settings settings;
+    settings.output_path = "/home/laptop/Desktop/out.wav";
+    settings.input_paths.emplace_back("/home/laptop/Desktop/animalese-generator/sounds/high/sound01.wav");
+    settings.input_paths.emplace_back("/home/laptop/Desktop/animalese-generator/sounds/high/sound01.wav");
+    settings.input_paths.emplace_back("/home/laptop/Desktop/animalese-generator/sounds/high/sound02.wav");
+    settings.input_paths.emplace_back("/home/laptop/Desktop/animalese-generator/sounds/high/sound02.wav");
+    settings.input_paths.emplace_back("/home/laptop/Desktop/animalese-generator/sounds/high/sound01.wav");
+    
+    Wav_File_Joiner::Join(settings);
+    
 }
