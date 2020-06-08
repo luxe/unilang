92c9beed821802a8d713746d1ece92d8f4746a58
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-06-07 23:35:49 -0400

    updates

--- source/code/hello_world/glfw/main.cpp
+++ source/code/hello_world/glfw/main.cpp
@@ -1,4 +1,5 @@
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -7,6 +8,7 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+    std::cout << "sdfsdf" << std::endl;
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
