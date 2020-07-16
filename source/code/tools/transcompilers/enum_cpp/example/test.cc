297abb2faf093d5a84294acfc31644562998ada3
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-07-15 21:52:03 -0400

    updates

--- source/code/tools/transcompilers/enum_cpp/example/test.cc
+++ source/code/tools/transcompilers/enum_cpp/example/test.cc
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <string>
+#include "prettyprint.hpp"
 #include "code/tools/transcompilers/enum_cpp/example/test_enum.hpp"
 
 int main(int argc, char** argv){
 
+    //This example will demonstrate all of the generated utility functions you get when using enum_cpp
+    
+    
 	Test_Enum e = Test_Enum::FOO;
-	std::cout << as_string(e) << std::endl;
+	std::cout << "as_string(e): " << as_string(e) << std::endl;
+    std::cout << "Test_Enum_all_strings(): " << Test_Enum_all_strings() << std::endl;
 }
