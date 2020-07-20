a61fadfb9dda89b0d586a320a63af9486c4c28b7
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-07-19 22:48:23 -0400

    updates

---
+++ source/code/tools/reflexivity/concept_tree.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "code/utilities/code/concept_tree_to_image.hpp"
+#include "code/utilities/filesystem/paths/lib.hpp"
+
+
+int main(int argc, char** argv){
+    
+    Move_To_Bazel_Derived_Path_To_Self_Unilang();
+    Concept_Tree_To_Image::Create();
+}
