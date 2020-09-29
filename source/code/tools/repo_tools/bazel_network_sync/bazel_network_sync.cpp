d372ad2f0e452a1f502231c9d81d4044d3178328
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-09-28 20:46:50 -0400

    updates

---
+++ source/code/tools/repo_tools/bazel_network_sync/bazel_network_sync.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "code/tools/repo_tools/bazel_network_sync/task_executer.hpp"
+#include "code/tools/repo_tools/bazel_network_sync/program_options/program_options.hpp"
+
+
+int main(int const argc, char** const argv){
+    
+    Program_Options program_options(argc, argv);
+    Task_Executer::Execute_Needed_Tasks(program_options);
+}
+
