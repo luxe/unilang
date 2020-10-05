b02a656a52ec5e42474bd49d2fbd6c646fe91d56
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-10-05 10:58:20 -0400

    updates

---
+++ source/code/tools/repo_tools/remote_execution/client/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include "code/tools/repo_tools/remote_execution/client/client.hpp"
+
+
+int main(int argc, char** argv) {
+    
+    Client::Run();
+}
