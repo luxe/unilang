4484085b8ec4be558ecc0b96009f50b793f431cb
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-10-06 12:41:14 -0400

    updates

--- source/code/hello_world/semver/main.cpp
+++ source/code/hello_world/semver/main.cpp
@@ -1,4 +1,5 @@
 #include "include/semver.hpp"
+#include <iostream>
 
 int main(){
     constexpr semver::version v1{1, 4, 3};
@@ -8,4 +9,11 @@ int main(){
     static_assert(v1 >= v2);
     static_assert(v2 < v1);
     static_assert(v2 <= v1);
+    
+    
+    semver::version ptest("1.2.3");
+    std::cout << int(ptest.major) << std::endl;
+    std::cout << std::to_string(ptest.minor) << std::endl;
+    std::cout << std::to_string(ptest.patch) << std::endl;
 }
+
