fe7932eba2cf7175d0a611b1a3927e0207446e85
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-10-07 23:11:08 -0400

    updates

--- source/code/external_projects/bf/running/buildfarm_start.sh
+++ source/code/external_projects/bf/running/buildfarm_start.sh
@@ -6,6 +6,6 @@ chmod 777 /tmp/worker;
 buildfarm_scripts/fix_redis.sh
 gnome-terminal --window -e 'buildfarm_scripts/start_redis.sh' \
 --tab -e 'buildfarm_scripts/start_gpu_shard_server.sh' \
---tab -e 'buildfarm_scripts/start_gpu_shard_worker.sh' \
+#--tab -e 'buildfarm_scripts/start_gpu_shard_worker.sh' \
 --tab -e 'buildfarm_scripts/start_shard_worker.sh' \
 --tab -e 'buildfarm_scripts/go_to_cache.sh'
