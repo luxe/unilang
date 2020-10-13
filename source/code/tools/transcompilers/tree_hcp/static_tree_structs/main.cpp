e11f7068e3f5318019ca16313e4be99dcbc12afb
Trevor Hickey <TrevorJamesHickey@gmail.com>
2020-10-13 01:38:13 -0400

    updates

---
+++ source/code/tools/transcompilers/tree_hcp/static_tree_structs/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "code/tools/transcompilers/tree_hcp/static_tree_structs/structs_creator.hpp"
+#include "code/tools/transcompilers/tree_hcp/static_tree_structs/program_options/program_options.hpp"
+#include "code/utilities/program/common_actions/locale/lib.hpp"
+
+int main(int argc, char** argv){
+    
+    //Unicode please
+    Set_English_UTF8_Locale();
+
+    //get program options
+    auto po = Program_Options(argc,argv);
+
+    //build it
+    Structs_Creator::Create(po.Input_File(),po.Output_File());
+
+}
