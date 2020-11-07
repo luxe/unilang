af5e5b55640f31a77534fc14e4f96247bcbffa3b
Trevor Hickey <thickey@uber.com>
2020-11-07 14:11:20 -0500

    updates

--- source/code/utilities/formats/json/converters/lib.hpp
+++ source/code/utilities/formats/json/converters/lib.hpp
@@ -7,17 +7,9 @@
 #include "code/utilities/streams/filestreams/lib.hpp"
 #include "code/utilities/streams/filestreams/read_all/lib.hpp"
 
-//json append for all types that don't derive from an hcp file construct
-//so we assume the json library can handle all basic types and std containers
-//the function will either return a basic type, a std container, or call an overload that returns a json
-template <typename T>
-T As_JSON_Assignable(T const& t){
-  return t;
-}
 template <typename T>
 std::string As_JSON_String(T const& t){
-    auto j = As_JSON_Assignable(t);
-    nlohmann::json r = j;
+    nlohmann::json r = t;
     return r.dump(2);
 }
 
@@ -30,14 +22,12 @@ T From_JSON_String(std::string const& str){
 
 template <typename T>
 std::string As_JSON_String_Indented_N_Spaces(T const& t, unsigned int const& number_of_spaces){
-    auto j = As_JSON_Assignable(t);
-    nlohmann::json r = j;
+    nlohmann::json r = t;
     return r.dump(number_of_spaces);
 }
 template <typename T>
 std::string As_Minified_JSON_String(T const& t){
-    auto j = As_JSON_Assignable(t);
-    nlohmann::json r = j;
+    nlohmann::json r = t;
     return r.dump();
 }
 template <typename T>
