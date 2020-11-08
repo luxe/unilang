2e91b47c16e8e1a8e1b87e86e2fdcdb86a19c1da
Trevor Hickey <thickey@uber.com>
2020-11-08 01:45:33 -0500

    updates

--- source/code/hello_world/json/patch/main.cpp
+++ source/code/hello_world/json/patch/main.cpp
@@ -2,11 +2,26 @@
 #include "nlohmann/json.hpp"
 #include "code/utilities/formats/json/patching/json_patch_builder.hpp"
 
-
-
-
+//this is an example of using some methods to build up a json patch.
+//a json patch is described in this RFC: https://tools.ietf.org/html/rfc6902
 int main(){
     
+
+    //first we construct an example json patch according to:
+    //https://tools.ietf.org/html/rfc6902#section-3
+    nlohmann::json rfc_example = R"(
+       [
+         { "op": "test", "path": "/a/b/c", "value": "foo" },
+         { "op": "remove", "path": "/a/b/c" },
+         { "op": "add", "path": "/a/b/c", "value": [ "foo", "bar" ] },
+         { "op": "replace", "path": "/a/b/c", "value": 42 },
+         { "op": "move", "from": "/a/b/c", "path": "/a/b/d" },
+         { "op": "copy", "from": "/a/b/d", "path": "/a/b/e" }
+       ]
+       
+    )"_json;
+    
+    //then we build the same thing using helper methods
     nlohmann::json patches;
     Json_Patch_Builder::Add_Test_Patch(patches,"/a/b/c","foo");
     Json_Patch_Builder::Add_Remove_Patch(patches,"/a/b/c");
@@ -15,5 +30,14 @@ int main(){
     Json_Patch_Builder::Add_Move_Patch(patches,"/a/b/c","/a/b/d");
     Json_Patch_Builder::Add_Copy_Patch(patches,"/a/b/d","/a/b/e");
     
+    //see the results
     std::cout << patches.dump(2) << std::endl;
+    
+    //ensure that we are matching the format of the RFC
+    if (rfc_example == patches){
+        std::cout << "match" << std::endl;
+    }
+    else{
+        std::cout << "DOES NOT match" << std::endl;
+    }
 }
