95347830f4852cbc0b0c3f24d50dc1e72471a95d
Trevor Hickey <thickey@uber.com>
2020-12-05 00:13:25 -0500

    updates

--- source/code/scratch/config/shell/zsh/src/custom/exports.zsh
+++ source/code/scratch/config/shell/zsh/src/custom/exports.zsh
@@ -1,4 +1,5 @@
-#I installed pycparser and needed this path:
-export PATH=/usr/local/lib/python2.7/dist-packages:$PATH
+#I installed pycparser and needed this path.
+#Now it breaks stuff so I removed it
+#export PATH=/usr/local/lib/python2.7/dist-packages:$PATH
 export PATH="$PATH:$HOME/bin"
 
---
+++ source/code/tools/puzzles/advent_of_code/2020/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "code/tools/puzzles/advent_of_code/2020/task_executer.hpp"
+
+int main(){
+    Task_Executer::Execute_Needed_Tasks();
+}
