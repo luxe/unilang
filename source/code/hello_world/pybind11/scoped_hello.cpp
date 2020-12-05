4abdc6a06fd4adfb1e2bfc33a6208609959a280b
Trevor Hickey <thickey@uber.com>
2020-12-05 00:47:53 -0500

    updates

--- source/code/hello_world/pybind11/scoped_hello.cpp
+++ source/code/hello_world/pybind11/scoped_hello.cpp
@@ -1,8 +1,7 @@
 #include <pybind11/embed.h> // everything needed for embedding
-namespace py = pybind11;
 
 int main() {
-    py::scoped_interpreter guard{}; // start the interpreter and keep it alive
+    pybind11::scoped_interpreter guard{}; // start the interpreter and keep it alive
 
-    py::print("Hello, World!"); // use the Python API
+    pybind11::print("Hello, World!"); // use the Python API
 }
