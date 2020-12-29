2184941d8a461b543623786186a55af4447084d9
Trevor Hickey <thickey@uber.com>
2020-12-29 15:17:56 -0500

    updates

--- source/code/tools/system/default_applications/main.cpp
+++ source/code/tools/system/default_applications/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
-#include "code/utilities/system/mime_xml_creator.hpp"
+#include "code/utilities/system/mime/mime_xml_creator.hpp"
+#include "code/utilities/system/mime/mime_xml_creation_settings.hpp"
 
 int main(){
     
-    std::cout << Mime_Xml_Creator::Create() << std::endl;
+    Mime_Xml_Creation_Settings settings;
+    settings.mime_type = "application/unilang";
+    settings.comment = "unilang file";
+    settings.file_extension = "unilang";
+    
+    std::cout << Mime_Xml_Creator::Create(settings);
 }
