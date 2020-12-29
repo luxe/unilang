445da1ca12a0736b6bb16d77ad000189c8e06d57
Trevor Hickey <thickey@uber.com>
2020-12-29 16:06:08 -0500

    updates

--- source/code/tools/system/default_applications/main.cpp
+++ source/code/tools/system/default_applications/main.cpp
@@ -4,10 +4,9 @@
 #include "code/utilities/filesystem/paths/lib.hpp"
 #include "code/utilities/filesystem/files/creating/lib.hpp"
 
-int main(){
-    
-    //https://askubuntu.com/questions/747467/create-new-filetype-with-existing-extension
-    
+void Create_Mime_Xml(){
+
+    //create mime xml
     Mime_Xml_Creation_Settings settings;
     settings.mime_type = "application/unilang";
     settings.comment = "unilang file";
@@ -20,4 +19,31 @@ int main(){
     Write_To_File(file_name,xml_contents);
     
     execute("update-mime-database ~/.local/share/mime");
+}
+
+void Create_Desktop_File(){
+
+    //create desktop file
+    std::string x;
+    x += "[Desktop Entry]\n";
+    x += "Name=Unilang\n";
+    x += "Exec=gedit\n";
+    x += "MimeType=application/unilang\n";
+    x += "Terminal=false\n";
+    x += "Type=Application\n";
+    
+    auto file_name = Full_Path_To_Home_File(".local/share/applications/unilang.desktop");
+    Create_File_Even_If_The_Path_Doesnt_Exist(file_name);
+    Write_To_File(file_name,x);
+    
+    execute("update-desktop-database ~/.local/share/applications");
+}
+
+
+int main(){
+    
+    //https://askubuntu.com/questions/747467/create-new-filetype-with-existing-extension
+    
+    Create_Mime_Xml();
+    Create_Desktop_File();
 }
