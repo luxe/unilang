4c5dcd05bb33c0416443fd70cefea8a11cda3c2c
Trevor Hickey <thickey@uber.com>
2021-01-10 02:25:06 -0500

    updates

---
+++ source/code/utilities/data_structures/graphics/image/image.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "code/utilities/data_structures/position/position.hpp"
+
+class Image
+{
+
+private:
+    unsigned char *img_data = nullptr;
+    GLuint         texture  = 0;
+    int            width    = 0;
+    int            height   = 0;
+    int            rows     = 0;
+
+public:
+    Image() {}
+    void set_image(std::string path)
+    {
+        free(img_data);
+        img_data = stbi_load(path.c_str(), &width, &height, &rows, 4);
+        // stbir_resize_uint8(img_data, width, height, 0)
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data);
+    }
+
+    ~Image() { free(img_data); }
+
+    void imgui_image() { ImGui::Image((void *) (intptr_t) texture, ImVec2(width, height)); }
+    void imgui_drawlist(ImDrawList *draw_list, Position pos)
+    {
+        draw_list->AddImage((void *) (intptr_t) texture, ImVec2(pos.x, pos.y), ImVec2(pos.x + width, pos.y + height));
+    }
+};
