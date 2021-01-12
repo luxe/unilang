5d91061a855346326098e8d3281cbbee70c70151
Trevor Hickey <thickey@uber.com>
2021-01-12 16:10:23 -0500

    updates

--- source/code/tools/transcompilers/code/code_to_java/main.cpp
+++ source/code/tools/transcompilers/code/code_to_java/main.cpp
@@ -4,6 +4,10 @@
 #include "code/utilities/formats/json/converters/json_file_io.hpp"
 #include "code/utilities/code/concept_static_tree_structs.hpp"
 #include "code/utilities/filesystem/files/creating/lib.hpp"
+#include "code/utilities/contributor/contributor.hpp"
+#include "code/utilities/linguistics/computer/frontends/java/settings/java_settings.hpp"
+#include "code/utilities/linguistics/computer/frontends/java/java_str_getter.hpp"
+#include "code/utilities/linguistics/computer/frontends/java/settings/java_default_rule_settings.hpp"
 
 int main(int argc, char** argv){
 
@@ -18,7 +22,9 @@ int main(int argc, char** argv){
     Json_File_Io::Read_Json_From_File(code,po.Input_File());
     
     //convert to java code
-    std::string str = "test";
+    std::vector<Contributor> contributors;
+    Java_Settings settings = Java_Default_Rule_Settings::Get();
+    std::string str = Java_Str_Getter::Get(code.module[0],contributors,settings);
     
     //export
     Write_To_File(po.Output_File(),str);
