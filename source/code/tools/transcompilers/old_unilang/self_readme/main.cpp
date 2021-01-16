94aab0444fd698c3c8affa3d8a46a08116203e31
Trevor Hickey <thickey@uber.com>
2021-01-15 19:36:04 -0500

    updates

--- source/code/tools/transcompilers/old_unilang/self_readme/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "code/tools/transcompilers/old_unilang/self_readme/unilang_documentation_generator.hpp"
-
-int main(){
-	Unilang_Documentation_Generator::Generate();
-}
