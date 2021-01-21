afa0b31524a26004e57aad4e635ef110c8532429
Trevor Hickey <thickey@uber.com>
2021-01-21 15:39:15 -0500

    updates

--- source/code/tools/transcompilers/old_unilang/structure/model/sections/structure_enums.hpp
+++ source/code/tools/transcompilers/old_unilang/structure/model/sections/structure_enums.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 //enums to use in place of strings to make code more strongly typed
-enum class Class_Access   { PUBLIC, PROTECTED, PRIVATE };
+//enum class Class_Access   { PUBLIC, PROTECTED, PRIVATE };
 enum class Value_Overload { LVALUE, RVALUE, NONE };
 enum class Storage_Specifier {STATIC, EXTERN, NONE };
 
