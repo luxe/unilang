3d72982a5baa28679248d9bcefdc7d615acda1ff
Trevor Hickey <thickey@uber.com>
2021-01-23 02:01:15 -0500

    updates

--- source/code/tools/obj_viewer/impl2/main.cpp
+++ source/code/tools/obj_viewer/impl2/main.cpp
@@ -140,9 +140,9 @@ void reshape(int w, int h) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     if (h == 0) {
-        //gluPerspective(80, (float) w, 1.0, 5000.0);
+        gluPerspective(80, (float) w, 1.0, 5000.0);
     } else {
-        //gluPerspective(80, (float) w / (float) h, 1.0, 5000.0);
+        gluPerspective(80, (float) w / (float) h, 1.0, 5000.0);
     }
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
