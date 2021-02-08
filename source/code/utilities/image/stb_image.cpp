83a02d69e5ca8557e669a3f29ef2e47bc77b3b32
Trevor Hickey <thickey@uber.com>
2021-02-08 12:27:11 -0500

    updates

--- source/code/utilities/image/stb_image.cpp
+++ source/code/utilities/image/stb_image.cpp
@@ -1,1 +1,2 @@
-//#include "code/utilities/image/stb_image.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include "code/utilities/image/stb_image.hpp"
--- source/code/utilities/image/stb_image.hpp
+++ source/code/utilities/image/stb_image.hpp
@@ -1,3 +1,2 @@
 #pragma once
-#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
