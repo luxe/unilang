85f83ac0b418856b675812016239b08ff04b6a7d
Trevor Hickey <thickey@uber.com>
2022-01-20 04:47:30 -0500

    updates

--- source/code/utilities/types/strings/creators/lib.hpp
+++ source/code/utilities/types/strings/creators/lib.hpp
@@ -2,12 +2,16 @@
 #include <string>
 #include <vector>
 
-std::string Create_String_Of_N_Chars(unsigned int const& num_of_chars, char const& c);
-std::string Create_String_Of_N_Strs(unsigned int const& num_of_chars, std::string const& c);
-std::string Create_String_Of_N_Spaces(unsigned int num_spaces);
+//by conversion
 std::string Char_To_String(unsigned char c);
 std::string Chars_To_String(std::vector<char> chars);
 
+//by repeating
+std::string Create_String_Of_N_Chars(unsigned int const& num_of_chars, char const& c);
+std::string Create_String_Of_N_Strs(unsigned int const& num_of_chars, std::string const& c);
+std::string Create_String_Of_N_Spaces(unsigned int num_spaces);
+
+//xml creation
 std::string Start_Tag(std::string const& tag_name, std::string const& key, std::string const& value);
 std::string Tag_Content(std::string const& tag_name,std::string const& content);
 std::string Single_End_Tag(std::string const& tag_name);
