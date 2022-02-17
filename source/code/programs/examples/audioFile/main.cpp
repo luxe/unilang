cfbc7b95d406fc011db91a8a7704a2410be5e25b
Trevor Hickey <thickey@uber.com>
2022-02-17 18:10:18 -0500

    updates

--- source/code/programs/examples/audioFile/main.cpp
+++ source/code/programs/examples/audioFile/main.cpp
@@ -21,6 +21,6 @@ int main(int argc, char** argv){
     
     //Pinyin_Speaker::Speak("Nǐ hǎo wǒ de míngzì shì",Full_Path_To_Desktop(),"out");
     
-    Pinyin_Speaker::Speak("Bàba",Full_Path_To_Desktop(),"out");
-    //Pinyin_Speaker::Speak("Bàba māmā hànbǎobāo",Full_Path_To_Desktop(),"out");
+    //Pinyin_Speaker::Speak("Bàba",Full_Path_To_Desktop(),"out");
+    Pinyin_Speaker::Speak("Bàba māmā hànbǎobāo",Full_Path_To_Desktop(),"out");
 }
