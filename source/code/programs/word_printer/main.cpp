d71f747cb8b2a63ee58d5ce4f633df51eda975c3
Trevor Hickey <thickey@uber.com>
2022-03-17 18:53:04 -0400

    updates

--- source/code/programs/word_printer/main.cpp
+++ source/code/programs/word_printer/main.cpp
@@ -1,6 +1,7 @@
 #include "code/utilities/linguistics/dictionary/three_letter_words.hpp"
 #include "code/utilities/linguistics/dictionary/wordle.hpp"
 #include "code/utilities/types/strings/transformers/casing/lib.hpp"
+#include "code/utilities/types/vectors/operators/lib.hpp"
 
 
 bool valid(std::string word, std::vector<std::string> const middle_words){
@@ -37,43 +38,44 @@ int main(){
 
 
     // print data
-    std::cout << "export const WORDS = [" << std::endl;
-    for (auto word: final_valids){
-        Lowercase(word);
-        std::cout << "'" << word << "'," << std::endl;
-    }
-    std::cout << "]\n\n";
-    
-    
-    
-    std::cout << "export const MIDDLE_WORDS = [" << std::endl;
-    for (auto word: answer_middle_words){
-        Lowercase(word);
-        std::cout << "'" << word << "'," << std::endl;
-    }
-    std::cout << "]\n\n";
-    
-    
-    
-    
-    
-    // auto guess_words = Wordle::Get_Allowed_Guesses();
-    // auto guess_middle_words = Three_Letter_Words::Get_Allowed_Guesses();
-    
-    
-    // std::cout << "export const VALID_GUESSES = [" << std::endl;
-    // for (auto word: guess_words){
+    // std::cout << "export const WORDS = [" << std::endl;
+    // for (auto word: final_valids){
     //     Lowercase(word);
     //     std::cout << "'" << word << "'," << std::endl;
     // }
     // std::cout << "]\n\n";
     
-    // std::cout << "export const VALID_MIDDLE_GUESSES = [" << std::endl;
-    // for (auto word: guess_middle_words){
+    
+    
+    // std::cout << "export const MIDDLE_WORDS = [" << std::endl;
+    // for (auto word: answer_middle_words){
     //     Lowercase(word);
     //     std::cout << "'" << word << "'," << std::endl;
     // }
     // std::cout << "]\n\n";
+    
+    
+    
+    
+    
+    auto guess_words = Wordle::Get_Allowed_Guesses();
+    guess_words += Wordle::Get_Answers();
+    auto guess_middle_words = Three_Letter_Words::Get_Allowed_Guesses();
+    
+    
+    std::cout << "export const VALID_GUESSES = [" << std::endl;
+    for (auto word: guess_words){
+        Lowercase(word);
+        std::cout << "'" << word << "'," << std::endl;
+    }
+    std::cout << "]\n\n";
+    
+    std::cout << "export const VALID_MIDDLE_GUESSES = [" << std::endl;
+    for (auto word: guess_middle_words){
+        Lowercase(word);
+        std::cout << "'" << word << "'," << std::endl;
+    }
+    std::cout << "]\n\n";
     
     
 
