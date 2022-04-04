8e0b0e0d0ecb0f22dfc1bc391ff9493220689fc4
Trevor Hickey <thickey@uber.com>
2022-04-03 21:54:42 -0400

    updates

--- source/code/utilities/types/strings/observers/search/lib.cpp
+++ source/code/utilities/types/strings/observers/search/lib.cpp
@@ -12,3 +12,22 @@ int countSubstring(const std::string& str, const std::string& sub)
     }
     return count;
 }
+
+long countSubstringOnRepeatingString(std::string str, long totalCharAmount, std::string const& subString){
+    
+     //the count we can see
+     long count = countSubstring(str,subString);
+    
+    //predicting future pattern
+    long howManyMore = (totalCharAmount / str.size()) - 1;
+    long extraLetterCount = totalCharAmount % str.size();
+    
+    //additional based on full duplicates
+    count += count*howManyMore;
+    
+    //additional based on partial duplicate
+    str = str.substr(0, extraLetterCount);
+    count += countSubstring(str,subString);
+    
+    return count;
+}
--- source/code/utilities/types/strings/observers/search/lib.hpp
+++ source/code/utilities/types/strings/observers/search/lib.hpp
@@ -1,3 +1,5 @@
 #pragma once
 
 int countSubstring(const std::string& str, const std::string& sub);
+
+long countSubstringOnRepeatingString(std::string const& str, long totalCharAmount, std::string const& subString);
