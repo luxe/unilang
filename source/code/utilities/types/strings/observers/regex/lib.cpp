c6795b0c4ca238a9f973ecc61d7a423a6940dfce
Trevor Hickey <thickey@uber.com>
2022-11-06 16:43:12 -0500

    updates

--- source/code/utilities/types/strings/observers/regex/lib.cpp
+++ source/code/utilities/types/strings/observers/regex/lib.cpp
@@ -5,3 +5,13 @@ bool Matches_Regex(std::string const& str, std::string const& regex){
     std::regex obj_regex(regex);
     return std::regex_match(str, obj_regex);
 }
+
+bool Matches_Any_Regex(std::string const& str, std::vector<std::regex> const& regexes){
+    
+    for (auto it: regexes){
+        if (std::regex_match(str, it)){
+            return true;
+        }
+    }
+    return false;
+}
--- source/code/utilities/types/strings/observers/regex/lib.hpp
+++ source/code/utilities/types/strings/observers/regex/lib.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
 #include <string>
+#include <regex>
 
 bool Matches_Regex(std::string const& str, std::string const& regex);
+
+bool Matches_Any_Regex(std::string const& str, std::vector<std::regex> const& regexes);
