6815049e202df7fcde319127f46e09601d9d5a2f
Trevor Hickey <thickey@uber.com>
2022-12-01 10:39:05 -0500

    updates

--- source/code/utilities/types/strings/observers/splitting/lib.cpp
+++ source/code/utilities/types/strings/observers/splitting/lib.cpp
@@ -94,6 +94,9 @@ std::vector<std::string> Split_Into_Parts_From_Fwd_Slash(std::string const& str)
 std::vector<std::string> Split_Into_Parts_From_Tab(std::string const& str){
     return Split_Into_Parts_From_Delimiter(str,'\t');
 }
+std::vector<std::string> Split_Into_Parts_From_Newline(std::string const& str){
+    return Split_Into_Parts_From_Delimiter(str,'\n');
+}
 std::string Split_Into_Parts_From_Fwd_Slash_Take_Last(std::string const& str){
     auto parts = Split_Into_Parts_From_Fwd_Slash(str);
     return parts.back();
@@ -136,9 +139,6 @@ std::vector<std::vector<std::string>> Split_By_Spaces(std::vector<std::string> c
 
 std::vector<std::string> Split_By_Spaces(std::string const& str){
  	return Split_Into_Parts_From_Delimiter(str,' ');
-}
-std::vector<std::string> Split_Into_Parts_From_Newline(std::string const& str){
-    return Split_Into_Parts_From_Delimiter(str,'\n');
 }
 std::vector<std::string> Split_Into_Parts_From_Dash(std::string const& str){
     return Split_Into_Parts_From_Delimiter(str,'-');
