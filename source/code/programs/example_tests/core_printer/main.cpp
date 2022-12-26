befa7c555e8814d1ed19248e110eb6f7ed3e2718
Trevor Hickey <thickey@uber.com>
2022-12-26 09:26:16 -0500

    updates

--- source/code/programs/example_tests/core_printer/main.cpp
+++ source/code/programs/example_tests/core_printer/main.cpp
@@ -2,6 +2,7 @@
 #include <memory>
 #include <iostream>
 int main(){
+    // prints number of cores detected by C++
     int threadNumbers = std::thread::hardware_concurrency();
     std::cout << "observed cores: " << threadNumbers << std::endl;
 }
