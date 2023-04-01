2055fd3c4d85e5eab27f30183906eb54ee59adbc
Trevor Hickey <thickey@uber.com>
2023-03-31 20:28:47 -0400

    updates

--- source/code/scratch/table/ag-grid/main.js
+++ source/code/scratch/table/ag-grid/main.js
@@ -1,4 +1,15 @@
+function rowSpan(params) {
+  var behaviourGroup = params.data ? params.data.behaviour_group : undefined;
+  if (behaviourGroup === 'Autonomy Envelope') {
+    return 3;
+  }
+    return 1;
+  }
+
 const columnDefs = [
+  { field: 'behaviour_group', headerName: 'Behaviour Group', rowSpan: rowSpan,
+    cellClassRules: {'show-cell': 'value !== undefined',},
+  },
   { field: 'behaviour', headerName: 'Behaviour'},
   { field: 'verification_method', headerName: 'Verification Method'},
   { field: 'owner', headerName: 'Owner'},
@@ -44,11 +55,13 @@ const columnDefs = [
       { field: 'test_execution_date', headerName: 'Due Date'},
     ],
   },
+  { field: 'mission_ready', headerName: 'Mission Ready'},
 ];
 
 // specify the data
 const rowData = [
   {
+    behaviour_group: "Autonomy Envelope",
     behaviour: "Vehicle Behaviors",
     verification_method: "DVP",
     owner: "NA",
@@ -60,7 +73,20 @@ const rowData = [
     test_execution_status: "X6", test_execution_date: "Y6",
   },
   {
+    //behaviour_group: "Autonomy Envelope",
     behaviour: "Bounding Objects / Scenarios (Perception)",
+    verification_method: "DVP",
+    owner: "NA",
+    requirement_status: "X", requirement_date: "Y",
+    verification_status: "X2", verification_date: "Y2",
+    test_case_status: "X3", test_case_date: "Y3",
+    data_status: "X4", data_date: "Y4",
+    impl_status: "X5", impl_date: "Y5",
+    test_execution_status: "X6", test_execution_date: "Y6",
+  },
+  {
+    //behaviour_group: "Autonomy Envelope",
+    behaviour: "Bounding Objects/Scenarios (Other)",
     verification_method: "DVP",
     owner: "NA",
     requirement_status: "X", requirement_date: "Y",
@@ -75,6 +101,7 @@ const rowData = [
 // let the grid know which columns and what data to use
 const gridOptions = {
   columnDefs: columnDefs,
+  suppressRowTransform: true,
   rowData: rowData,
   defaultColDef: {
     editable: true,
